1e3e9b05fe30e984f0efcb41a571f74ad62c427e
Milkweedd <antontab91@gmail.com>
2020-03-04 19:37:52 +0200

    as

--- lesson2/task3/index.js
+++ lesson2/task3/index.js
@@ -3,14 +3,14 @@ let anotherNumber = goodNumber;
 anotherNumber = 77;
 console.log(goodNumber);
 console.log(anotherNumber);
-let custumer = {
+const customer = {
     name: 'John',
     age: 17
-};
-let anotherCustumer = custumer;
-anotherCustumer.age = 20;
-console.log(custumer.age);
-console.log(anotherCustumer.age);
+}
+const anotherCustomer = customer;
+anotherCustomer.age = 20;
+console.log(customer.age);
+console.log(anotherCustomer.age);
 
 
 
