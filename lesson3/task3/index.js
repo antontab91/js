81121f3f59f67ea1a45447d972227d050901f8f7
Milkweedd <antontab91@gmail.com>
2020-03-05 13:50:35 +0200

    sa

---
+++ lesson3/task3/index.js
@@ -0,0 +1,28 @@
+/* сравнение чисел */
+
+const a = 17;
+const b = 5;
+a > b;
+a <= b;
+a == b;
+a != b;
+
+console.log("a > b: ", a > b);
+console.log("a <= b: ", a <= b);
+console.log("a == b: ", a == b);
+console.log("a != b: ", a != b);
+
+
+/* сравнение строк */
+
+'a' < 'b';
+'ab' > 'a';
+console.log("'a' < 'b': ", 'a' < 'b');
+console.log("'ab' > 'a': ", 'ab' > 'a');
+
+/* строгое сравнение */
+
+17 === 1;
+17 === 17;
+console.log("17 === 1;: ", 17 === 1);
+console.log("17 === 17: ", 17 === 17);
