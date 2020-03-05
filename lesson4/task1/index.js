9bfb207b893460a863532b3e2b8a2e7969619b50
Milkweedd <antontab91@gmail.com>
2020-03-05 23:26:02 +0200

    sa

--- lesson4/task1/index.js
+++ lesson4/task1/index.js
@@ -1,7 +1,16 @@
 let n = 200;
 let sum = 0;
-while (n > 0) {
-    sum = sum + n;
-    n = n - 1;
+
+
+// while (n > 0) {
+//     sum = sum + n;
+//     console.log(sum);
+//     n = n - 1;
+//     console.log(n);
+// }
+
+for (let i = 0; i <= n; i++) {
+    sum = sum + i;
 }
+
 console.log('Result: ' + sum);
