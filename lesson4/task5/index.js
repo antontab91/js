4206f5d42dc7246e0347132b824d62bac4518970
Milkweedd <antontab91@gmail.com>
2020-03-06 17:15:08 +0200

    ыф

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -9,7 +9,9 @@ for (let i = m; i <= n; i++) {
         result = result - i;
     } else if (i % 4 == 0) {
         result = result * i;
-    } else if (i % 5 == 0) {
+    }
+
+    if (i % 5 == 0) {
         console.log(i);
     }
 }
