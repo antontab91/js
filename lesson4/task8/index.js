b6b15f78ed83d5ab59bba9e2a27d4bb760c4fe17
Milkweedd <antontab91@gmail.com>
2020-03-07 00:25:45 +0200

    sa

---
+++ lesson4/task8/index.js
@@ -0,0 +1,12 @@
+
+let sum = 0;    // обьявил переменную суммы
+for (let i = 1; i <= 1000; i++) {  // посчитал сумму от 0 до 1000
+    sum += i;
+}
+
+let integerPart = sum / 1234; //посчитал целую часть от деления от суммы на число 1234
+console.log(integerPart)
+let remainder = sum % 1234; // посчитал остаток от деления 
+console.log(remainder)
+
+console.log(integerPart > remainder); // сравнил остаток от деления с целой частью от деления 
