941fd7c8e6baf71b928682ab5add1bc5537f3727
Milkweedd <antontab91@gmail.com>
2020-03-06 18:35:17 +0200

    ФЫ

--- lesson4/task6/index.js
+++ lesson4/task6/index.js
@@ -1,14 +1,14 @@
 let sum = 0;
 
 for (let i = 0; i <= 1000; i++) {
+    sum = sum + i;
     if (i % 2 != 0) {
         console.log('Found');
-        sum = sum + i;
-        if (sum * 5 > 5000) {
-            console.log('Bigger');
-        }
-        else {
-            console.log('Smaller');
-        }
+    }
+    if (sum * 5 > 5000) {
+        console.log('Bigger');
+    }
+    else {
+        console.log('Smaller');
     }
 }
