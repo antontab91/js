201df990065b06f8ddf03cc51ee16cd79da36216
Milkweedd <antontab91@gmail.com>
2020-03-10 01:29:57 +0200

    as

--- lesson6/task9/index.js
+++ lesson6/task9/index.js
@@ -18,7 +18,8 @@ function reverseArray(arr) {
     let newArr = [];
 
     for (let i = arr.length - 1; i >= 0; i--) {
-        newAr.push(arr[i]);
+        newArr.push(arr[i]);
+
     }
 
     return newArr;
