0305495081fd89ed663eb0f9694a18f9cf0f896f
Milkweedd <antontab91@gmail.com>
2020-03-09 19:59:46 +0200

    sa

---
+++ lesson6/task3/index.js
@@ -0,0 +1,11 @@
+function getSpecialNumbers(m, n) {
+    let newArr = [];
+    for (let i = m; i <= n; i++) {
+        if (i % 3 === 0) {
+            newArr.push(i);
+        }
+    }
+    return newArr;
+}
+
+console.log(getSpecialNumbers(12, 45));
