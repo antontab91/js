2e4893922e7197651162b333d7ab6d2d451ce8e7
Milkweedd <antontab91@gmail.com>
2020-03-10 01:28:29 +0200

    as

--- lesson6/task9/index.js
+++ lesson6/task9/index.js
@@ -1,12 +1,29 @@
 const arr = [656, 1, 545, 87, 8787, 52];
 
+// function reverseArray(arr) {
+//     if (!Array.isArray(arr)) {
+//         return null;
+//     }
+//     let newArr = arr.reverse();
+//     return newArr;
+// }
+
+
+
+
 function reverseArray(arr) {
     if (!Array.isArray(arr)) {
         return null;
     }
-    let newArr = arr.reverse();
+    let newArr = [];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        newAr.push(arr[i]);
+    }
+
     return newArr;
 }
+
 reverseArray(arr);
 console.log(arr);
 console.log(reverseArray(arr));
