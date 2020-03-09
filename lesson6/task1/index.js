612506e3e6764f80113456c3c4f4539bde1b5f14
Milkweedd <antontab91@gmail.com>
2020-03-09 18:38:24 +0200

    sa

--- lesson6/task1/index.js
+++ lesson6/task1/index.js
@@ -1,7 +1,11 @@
+let arr = [12, 45, 45, 45, 78, 15, 12, 545]
+
 function getArrayBounds(arr) {
-    if (!arr.isArray) {
+    if (!Array.isArray(arr)) {
         return null;
     }
     return [arr.length, arr[0], arr[arr.length - 1]];
 }
+
+console.log(getArrayBounds(arr));
 
