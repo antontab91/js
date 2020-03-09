2234b7c8e2694dfdb50fa65a8e895e2b6ee1eb44
Milkweedd <antontab91@gmail.com>
2020-03-10 01:34:41 +0200

    фы

--- lesson6/task9/index.js
+++ lesson6/task9/index.js
@@ -1,3 +1,5 @@
+import { join } from "path";
+
 const arr = [656, 1, 545, 87, 8787, 52];
 
 // function reverseArray(arr) {
@@ -9,20 +11,18 @@ const arr = [656, 1, 545, 87, 8787, 52];
 // }
 
 
-
-
-function reverseArray(arr) {
-    if (!Array.isArray(arr)) {
+function reverseArray(arr) {   // обьявил функцию , которая принимает массмив
+    if (!Array.isArray(arr)) {  // проверка массив или нет 
         return null;
     }
-    let newArr = [];
+    let newArr = [];              // создал массив в который буду записывать элементы наоборот 
 
-    for (let i = arr.length - 1; i >= 0; i--) {
-        newArr.push(arr[i]);
+    for (let i = arr.length - 1; i >= 0; i--) {   // перебераю в обратную сторону 
+        newArr.push(arr[i]);                      // пушу по порядку в новій массив каждую итерацию цикла наоборот 
 
     }
 
-    return newArr;
+    return newArr;                                  // возвращаю перевернутый массив
 }
 
 reverseArray(arr);
