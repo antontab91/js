791e1c9ca613e744fa945549f0a1bf28ca8614eb
Milkweedd <antontab91@gmail.com>
2020-03-10 13:32:14 +0200

    as

--- lesson6/task15/index.js
+++ lesson6/task15/index.js
@@ -4,6 +4,10 @@ let balances = [1400, 87, -6];
 
 function withdraw(clients, balances, client, amount) {
     for (let i = 0; i < clients.length; i++) {
+        if (balances[i] < amount) {
+            console.log("assa");
+            return -1;
+        }
         if (clients[i] === client) {
             balances[i] = balances[i] - amount;
             console.log(balances[i]);
@@ -12,5 +16,5 @@ function withdraw(clients, balances, client, amount) {
     return balances;
 }
 
-withdraw(clients, balances, 'John', 50);
+withdraw(clients, balances, 'John', 95);
 console.log(balances);
