dbfa96a077beaea4e04c64404fb8d257f3c19678
Milkweedd <antontab91@gmail.com>
2020-03-10 02:07:09 +0200

    sa

--- lesson6/task12/index.js
+++ lesson6/task12/index.js
@@ -3,4 +3,10 @@ function sortAsc(array) {
     return array.sort(function (a, b) { return b - a }).reverse();
 }
 
+function sortDesc(array) {
+    return array.sort(function (a, b) { return b - a });
+}
+
+
 console.log(sortAsc(array));
+console.log(sortDesc(array));
