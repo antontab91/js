1683f8320e13a2919ad80fc66e993a9bc674e30b
Milkweedd <antontab91@gmail.com>
2020-03-10 10:46:25 +0200

    sd

---
+++ lesson6/task14/index.js
@@ -0,0 +1,23 @@
+let array = [0.32, 0.32, 45, 454, 45, 4545, 45];
+
+function uniqueCount(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!inArray(result, array[i])) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+
+function inArray(array, elem) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === elem) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log(uniqueCount(array));
