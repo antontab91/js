cbc556ae917de34cfc045cc165aabd8313efe1eb
Milkweedd <antontab91@gmail.com>
2020-03-10 23:38:10 +0200

    as

--- lesson7/task2/index.js
+++ lesson7/task2/index.js
@@ -1,7 +1,7 @@
 let arr = [232, 5645, 4, 4152, 12, 87, 7498, 54, 451, 422];
 
 function sortDesc(arr) {
-    return arr.sort(function (a, b) {
+    return arr.slice().sort(function (a, b) {
         return a - b;
     }).reverse();
 }
