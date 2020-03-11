0136ef44fa05ed5f7ba621d1a6c8b6dfeb9e1aa2
Milkweedd <antontab91@gmail.com>
2020-03-11 23:43:49 +0200

    as

--- lesson7/task12/index.js
+++ lesson7/task12/index.js
@@ -2,7 +2,7 @@ arr = [2, 5, 6, 3];
 
 function arrAverage(arr) {
     if (!Array.isArray(arr)) {
-        return -1;
+        return null;
     }
     let sum = arr.reduce(function (acc, num) {            // присвоил сумму переменной , сумму всего массива ищу с помощью метрода редьюс 
         return acc + num;
