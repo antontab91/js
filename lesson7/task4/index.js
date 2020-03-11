93652b0ff77815f089f153c8ceb10ce2375ec708
Milkweedd <antontab91@gmail.com>
2020-03-11 11:08:22 +0200

    ІВ

--- lesson7/task4/index.js
+++ lesson7/task4/index.js
@@ -1,4 +1,15 @@
-const getMessagesForBestStudents = (students, notPassed) =>
+let allStudents = ['Ann', 'Tom', 'Bob', 'Kate'];
+let notPassed = ['Tom', 'Bob'];
+
+const getMessagesForBestStudents = (allStudents, notPassed) =>
     students
         .filter(name => notPassed.indexOf(name) === -1)
         .map(item => 'Good job, ' + item);
+
+
+// function getMessagesForBestStudentsO(allStudents, notPassed) {
+//     const passedStudents = allStudents.filter(name => (notPassed.indexOf(name) === -1))
+//     return passedStudents;
+// }
+
+console.log(getMessagesForBestStudentsO(allStudents, notPassed));
