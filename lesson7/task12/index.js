6a76c8358e03d0d523c57659a3f4f3cd57e76ddc
Milkweedd <antontab91@gmail.com>
2020-03-11 23:46:30 +0200

    sa

--- lesson7/task12/index.js
+++ lesson7/task12/index.js
@@ -1,13 +1,17 @@
 arr = [2, 5, 6, 3];
 
-function arrAverage(arr) {
-    if (!Array.isArray(arr)) {
-        return null;
-    }
-    let sum = arr.reduce(function (acc, num) {            // присвоил сумму переменной , сумму всего массива ищу с помощью метрода редьюс 
-        return acc + num;
-    })
-    return sum / arr.length;                             // чтобы найти общее кратное делю сумму на длинну массива 
+// function arrAverage(arr) {
+//     if (!Array.isArray(arr)) {
+//         return null;
+//     }
+//     let sum = arr.reduce(function (acc, num) {            // присвоил сумму переменной , сумму всего массива ищу с помощью метрода редьюс 
+//         return acc + num;
+//     })
+//     return sum / arr.length;                             // чтобы найти общее кратное делю сумму на длинну массива 
+// }
+
+function arrAverage(nums) {
+    return nums.reduce((a, b) => (a + b)) / nums.length;
 }
 
 
