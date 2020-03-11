cd74b03fdc29495807df7575f13c37cedaef87d6
Milkweedd <antontab91@gmail.com>
2020-03-11 15:00:25 +0200

    sa

---
+++ lesson7/task6/index.js
@@ -0,0 +1,10 @@
+arr = [45, 5, 45, 12, 485, 87, 87845, 45, 12, 56, 896, 89];
+
+function reverseArray(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    return arr.reverse();
+}
+
+console.log(reverseArray(arr));
