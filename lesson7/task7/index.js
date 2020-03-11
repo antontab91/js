f699a7b1a7781bc417b4cbb2b02e8528dae053dc
Milkweedd <antontab91@gmail.com>
2020-03-11 16:34:43 +0200

    as

--- lesson7/task7/index.js
+++ lesson7/task7/index.js
@@ -1,10 +1,13 @@
 let arr = [1, 2, 5, 45, 12, 4, 7, 78, 3, 12, 56, 96, 89];
 
 function increaseEvenEl(arr, delta) {
+    if (!Array.isArray(arr)) {
+        return null
+    }
 
     return arr.map(function (elem, index, wholeArr) {
         if (elem % 2 === 0) {
-            return wholeArr[index] = elem + delta;
+            return elem + delta;
         } else {
             return elem;
         }
