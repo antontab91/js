9782e0899e40535ff2ff23e8876c732bd37974cf
Milkweedd <antontab91@gmail.com>
2020-03-11 18:29:31 +0200

    as

--- lesson7/task9/index.js
+++ lesson7/task9/index.js
@@ -1,12 +1,25 @@
 let clients = ['Ann', 'John', 'User'];
 let balances = [1400, 87, -6];
 
-function withdraw(clients, balances, client, amount) {    //  функция принимает массив клиентов , массив счетов , идентификатор клиента и сумму которую должен я отнять 
-    let currentIndex = clients.indexOf(client);             // обьявляю переменную Индекс которая равна индексу клиента 
-    const difference = balances[currentIndex] - amount;         // обьявляю переменную Диференс куда записываю разницу счета клиента с суммой, которую нужно отнять
-    if (difference < 0) {                                       // проверка если сумма снятия меньше чем есть на счете - ретурн -1
-        return -1
-    }
-    balances[currentIndex] = difference;                        // записываю в нужный элемент массива разницу 
-    return balances[currentIndex];                              // возвращаю измененный элемент массива
+// function withdraw(clients, balances, client, amount) {    //  функция принимает массив клиентов , массив счетов , идентификатор клиента и сумму которую должен я отнять 
+//     let currentIndex = clients.indexOf(client);             // обьявляю переменную Индекс которая равна индексу клиента 
+//     const difference = balances[currentIndex] - amount;         // обьявляю переменную Диференс куда записываю разницу счета клиента с суммой, которую нужно отнять
+//     if (difference < 0) {                                       // проверка если сумма снятия меньше чем есть на счете - ретурн -1
+//         return -1
+//     }
+//     balances[currentIndex] = difference;                        // записываю в нужный элемент массива разницу 
+//     return balances[currentIndex];                              // возвращаю измененный элемент массива
+// }
+
+
+function withdraw(clients, balances, client, amount) {
+    let currentIndex = clients.indexOf(client);
+    // console.log(currentIndex)
+    let diference = balances[currentIndex] - amount;
+    balances[currentIndex] = diference;
+    return balances[currentIndex];
 }
+
+console.log(
+    withdraw(clients, balances, 'John', 95)
+)
