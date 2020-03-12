fa7b372e1b32e6cd2a92f577be267296f4475071
Milkweedd <antontab91@gmail.com>
2020-03-12 20:37:16 +0200

    ыф

--- lesson9/task1/index.js
+++ lesson9/task1/index.js
@@ -5,19 +5,22 @@ const obj = {
 };
 
 
-function getAdults(obj) {
-    const usersArray = Object.entries(obj);
-    const filteredUsers = usersArray.filter(function (user) {
-        if (user[1] >= 18) {
-            return user;
-        }
-    })
-    const userNames = filteredUsers.map(function (user) {
-        return user[0];
-    })
-    return userNames;
+// function getAdults(obj) {
+//     const usersArray = Object.entries(obj);
+//     const filteredUsers = usersArray.filter(function (user) {
+//         if (user[1] >= 18) {
+//             return user;
+//         }
+//     })
+//     const userNames = filteredUsers.map(function (user) {
+//         return user[0];
+//     })
+//     return userNames;
+// }
 
-}
+const getAdults = userObj => Object.entries(userObj)
+    .filter(user => user[1] >= 18)
+    .map(user => user[0]);
 
 console.log(getAdults(obj));
 
