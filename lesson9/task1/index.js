88abfa1a9470df63ca5e9174cfe0a79a16b40a86
Milkweedd <antontab91@gmail.com>
2020-03-12 20:01:25 +0200

    фы

--- lesson9/task1/index.js
+++ lesson9/task1/index.js
@@ -1,37 +1,23 @@
-// const obj = {
-//     'John Doe': 19,
-//     'Tom': 17,
-//     'Bob': 18
-// };
+const obj = {
+    'John Doe': 19,
+    'Tom': 17,
+    'Bob': 18
+};
 
 
-// function getAdults(obj) {
-//     let usersArray = Object.entries(obj);
-//     let filteredUsers = usersArray.filter(function (user) {
-//         if (user[1] >= 18) {
-//             return user;
-//         }
-//     })
-//     let userNames = filteredUsers.map(function (user) {
-//         return user[0];
-//     })
-//     return userNames;
+function getAdults(obj) {
+    let usersArray = Object.entries(obj);
+    let filteredUsers = usersArray.filter(function (user) {
+        if (user[1] >= 18) {
+            return user;
+        }
+    })
+    let userNames = filteredUsers.map(function (user) {
+        return user[0];
+    })
+    return userNames;
 
-// }
+}
 
+console.log(getAdults(obj));
 
-
-
-// function getAdults(obj) {
-//     let userNames = Object.entries(obj)
-//         .filter(person => person[1] >= 18)
-//         .map(name => name[0]);
-//     return userNames;
-// }
-
-
-// console.log(getAdults({}));
-
-const getAdults = usersObj => Object.entries(usersObj)
-    .filter(user => user[1] >= 18)
-    .map(user => user[0]);
