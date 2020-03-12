a4de1627423ccd17a8cd3a826d2a50d6cf0e9e25
Milkweedd <antontab91@gmail.com>
2020-03-12 19:57:55 +0200

    sa

--- lesson9/task1/index.js
+++ lesson9/task1/index.js
@@ -22,16 +22,12 @@
 
 
 
-// function getAdults(obj) {
-//     let userNames = Object.entries(obj)
-//         .filter(person => person[1] >= 18)
-//         .map(name => name[0]);
-//     return userNames;
-// }
-
-const getAdults = obj => {
-    const arr = Object.entries(obj);
-    return arr
-        .filter(item => item[1] >= 18)
-        .map(elem => elem[0]);
+function getAdults(obj) {
+    let userNames = Object.entries(obj)
+        .filter(person => person[1] >= 18)
+        .map(name => name[0]);
+    return userNames;
 }
+
+
+console.log(getAdults({}));
