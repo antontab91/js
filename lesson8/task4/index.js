e87279779931ab95b86ccd6ee1492f6f16c8a4f8
Milkweedd <antontab91@gmail.com>
2020-03-12 18:45:38 +0200

    as

---
+++ lesson8/task4/index.js
@@ -0,0 +1,19 @@
+let obj = {
+    'John Doe': 19,
+    'Tom': 17,
+    'Bob': 18
+};
+
+
+function getAdults(obj) {
+    let newObj = {};
+    for (key in obj) {
+        console.log(obj[key]);
+        if (obj[key] >= 18) {
+            newObj[key] = obj[key];
+        }
+    }
+    return newObj;
+}
+
+console.log(getAdults(obj));
