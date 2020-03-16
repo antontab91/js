aada39afb9ea5693848a55e839b8c58e126b3586
Milkweedd <antontab91@gmail.com>
2020-03-17 00:02:57 +0200

    sa

--- lesson8/task8/index.js
+++ lesson8/task8/index.js
@@ -1,9 +1,6 @@
-const user = {
+const userData = {
     name: 'Sam'
 };
-
-
-
 
 function addPropertyV1(userData, userId) {
     userData.id = userId;
@@ -15,13 +12,19 @@ function addPropertyV2(userData, userId) {
 };
 
 function addPropertyV3(userData, userId) {
-    let id = userId;
+    let id = { id: userId };
     return Object.assign({}, userData, id);      // тут  векрнул новый обьейкт без мутации старого 
 };
 
 
 function addPropertyV4(userData, userId) {
-    let id = userId;
+    let id = { id: userId };
     return Object.assign({ ...userData }, id);   // tut spred operator
 };
 
+
+
+console.log(addPropertyV1(userData, 4545));
+console.log(addPropertyV2(userData, 4545));
+console.log(addPropertyV3(userData, 4545));
+console.log(addPropertyV4(userData, 4545));
