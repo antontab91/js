12f234102d35c7da4b876749ff5b79a775839739
Milkweedd <antontab91@gmail.com>
2020-03-17 01:18:49 +0200

    sa

---
+++ lesson8/task12/index.js
@@ -0,0 +1,30 @@
+const obj1 = {
+    name: 'Tom',
+    age: 17
+};
+const obj2 = {
+    name: 'Bob',
+    age: 17
+};
+const obj3 = {
+    name: 'Bob',
+    age: 17,
+    student: false
+};
+const obj4 = {
+    name: 'Tom',
+    age: 17,
+};
+
+function compareObjects(obj1, obj2) {
+    if (obj1 === obj2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+console.log(compareObjects(obj1, obj2)); // false
+console.log(compareObjects(obj2, obj3)); // false
+console.log(compareObjects(obj1, obj4)); // true
