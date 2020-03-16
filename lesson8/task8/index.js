f70710b762d36ef16979a6c1465cf6610699a1bc
Milkweedd <antontab91@gmail.com>
2020-03-16 23:28:18 +0200

    sd

---
+++ lesson8/task8/index.js
@@ -0,0 +1,18 @@
+const userData = {};
+
+function addPropertyV1(userData, userId) {
+
+    return userData.userId;
+
+}
+function addPropertyV2(userData, userId) {
+
+}
+function addPropertyV3(userData, userId) {
+
+}
+function addPropertyV4(userData, userId) {
+
+}
+
+console.log(addPropertyV1(userData))
