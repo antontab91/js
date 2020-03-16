d9090017421cc1223dedb3ba3e89c950cff38999
Milkweedd <antontab91@gmail.com>
2020-03-17 00:25:12 +0200

    sd

--- lesson8/task9/index.js
+++ lesson8/task9/index.js
@@ -4,22 +4,28 @@ const transaction = {
 
 
 function addPropertyV1(obj, key, value) {
-    obj.key = value;
+    obj[key] = value;
     return obj;
 };
 
 function addPropertyV2(obj, key, value) {
-
+    return Object.assign(obj, { key: value });
 };
 
 function addPropertyV3(obj, key, value) {
-
+    return Object.assign({}, obj, { key: value });
 };
 
 function addPropertyV4(obj, key, value) {
-
+    return Object.assign({ ...transaction }, { key: value });
 };
 
 
-const result = addPropertyV1(transaction, 'currency', 'USD');
-console.log(result); // { value: 170, currency: 'USD' }
+// console.log(addPropertyV1(transaction, 'currency', 'USD'));
+
+// console.log(addPropertyV2(transaction, 'currency', 'USD'));
+
+// console.log(addPropertyV3(transaction, 'currency', 'USD'));
+
+// console.log(addPropertyV1(transaction, 'currency', 'USD'));
+
