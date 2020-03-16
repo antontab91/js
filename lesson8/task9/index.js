d70892e8f5244496ee5fe5f4237b808aab13972f
Milkweedd <antontab91@gmail.com>
2020-03-17 00:10:38 +0200

    sa

---
+++ lesson8/task9/index.js
@@ -0,0 +1,16 @@
+function addPropertyV1(obj, key, value) {
+    obj[key] = value;
+};
+
+function addPropertyV2(obj, key, value) {
+
+};
+
+function addPropertyV3(obj, key, value) {
+
+};
+
+function addPropertyV4(obj, key, value) {
+
+};
+
