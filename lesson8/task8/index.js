153c71d70898f13fdc75a57d2b124c420f7600a5
Milkweedd <antontab91@gmail.com>
2020-03-16 23:46:15 +0200

    as

--- lesson8/task8/index.js
+++ lesson8/task8/index.js
@@ -1,18 +1,23 @@
 const userData = {};
 
 function addPropertyV1(userData, userId) {
-
-    return userData.userId;
-
+    userData.id = userId;
+    return userData;
 }
-function addPropertyV2(userData, userId) {
 
+function addPropertyV2(userData, userId) {
+    return Object.assign(userData, { id: userId })
 }
 function addPropertyV3(userData, userId) {
-
+    let newObj = { ...userData };
+    return Object.assign(newObj, userId);
 }
 function addPropertyV4(userData, userId) {
 
 }
 
-console.log(addPropertyV1(userData))
+
+console.log(addPropertyV1(userData, 4545));
+console.log(addPropertyV2(userData, 4545));
+console.log(addPropertyV3(userData, 4545));
+console.log(addPropertyV4(userData, 4545));
