8ea5799cc57f6861cc70b1fbbbc4315ed4da4085
Milkweedd <antontab91@gmail.com>
2020-03-17 00:15:33 +0200

    sa

--- lesson8/task9/index.js
+++ lesson8/task9/index.js
@@ -1,5 +1,11 @@
+const transaction = {
+    value: 170
+}
+
+
 function addPropertyV1(obj, key, value) {
     obj.key = value;
+    return obj;
 };
 
 function addPropertyV2(obj, key, value) {
@@ -14,3 +20,6 @@ function addPropertyV4(obj, key, value) {
 
 };
 
+
+const result = addPropertyV1(transaction, 'currency', 'USD');
+console.log(result); // { value: 170, currency: 'USD' }
