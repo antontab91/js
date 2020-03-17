c2a4e72076d787862940e3d9048208553b924b69
Milkweedd <antontab91@gmail.com>
2020-03-17 02:35:44 +0200

    sa

--- lesson8/task12/index.js
+++ lesson8/task12/index.js
@@ -18,32 +18,12 @@ const obj4 = {
 };
 
 function compareObjects(obj1, obj2) {
-    // console.log(JSON.stringify(obj1) === JSON.stringify(obj2));  // в гугле подсмотрел  Object.toJSON(user1) == Object.toJSON(user2);  , но  Object.toJSON не заработало потом мдн  https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
-    // return (JSON.stringify(obj1) === JSON.stringify(obj2));
-
-    let newObj1 = Object.entries(obj1).flat();
-    // console.log(newObj1);
-    let newObj2 = Object.entries(obj2).flat();
-    // console.log(newObj2);
-    if (newObj1.length !== newObj2.length) {
-        return false;
-    }
-    for (let i = 0; i < obj2.length; i++) {
-        console.log(obj1[i])
-        if (obj1[i] !== obj2[i]) {
-            return false;
-        }
-    }
-    return true;
-
-    // console.log(newObj1.length);
-    // console.log(newObj2.length);
-    // console.log((newObj1.length === newObj2.length));
+    console.log(JSON.stringify(obj1) === JSON.stringify(obj2));  // в гугле подсмотрел  Object.toJSON(user1) == Object.toJSON(user2);  , но  Object.toJSON не заработало потом мдн  https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+    return (JSON.stringify(obj1) === JSON.stringify(obj2));
 
 
 }
 
-
 console.log(compareObjects(obj1, obj2)); // false/
-// console.log(compareObjects(obj2, obj3)); // false
-// console.log(compareObjects(obj1, obj4)); // true/
+console.log(compareObjects(obj2, obj3)); // false
+console.log(compareObjects(obj1, obj4)); // true/
