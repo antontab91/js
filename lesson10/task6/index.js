764982c5c29caf73b1a459f5d60cbe8179e1b3fd
Milkweedd <antontab91@gmail.com>
2020-03-18 01:49:01 +0200

    as

--- lesson10/task6/index.js
+++ lesson10/task6/index.js
@@ -1,5 +1,5 @@
 function getRandomNumbers(len, min, max) {
-    if ((Math.ceil(max) === Math.ceil(min) && (max - min < 1)) || length <= 0 || max < min) {
+    if ((Math.ceil(max) === Math.ceil(min) && (max - min < 1)) || len <= 0 || max < min) {
         return null;
     }
 
