36d8713ae55ec6f8593a30524ffc7316a73ceb5c
Milkweedd <antontab91@gmail.com>
2020-03-17 23:17:35 +0200

    as

--- lesson10/task7/index.js
+++
@@ -1,9 +0,0 @@
-let arr = [213, 1, 12.12, -412.12, -54, 45, -44.545];
-
-function getTotalPrice(arr) {
-    return arr.reduce(function (acc, num) {
-        return acc + Math.abs(num);
-    })
-}
-
-getTotalPrice(arr);
