baea508b3e971254f2a521fd4d4cde9f841f4e9c
Milkweedd <antontab91@gmail.com>
2020-03-18 01:14:55 +0200

    sa

--- lesson10/task6/index.js
+++ lesson10/task6/index.js
@@ -1,5 +1,13 @@
-function getRandomNumbers(l, min, max) {
+function getRandomNumbers(len, min, max) {
     let result = [];
+    masLen = Math.round(len);
+    console.log(masLen);
 
+    for (let i = 0; i < masLen; i++) {
+        result.push(Math.random() * (max - min) + min);
+    }
 
+    console.log(result);
 }
+
+getRandomNumbers(2, 45, 54);
