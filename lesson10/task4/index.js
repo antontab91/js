92df2426462955e6bc747243b49e5adf6cdc9d85
Milkweedd <antontab91@gmail.com>
2020-03-17 22:57:36 +0200

    as

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,17 +1,36 @@
 let arr = [65, 62, -78.54, -56, -212, 45.12];
 
+// function getMaxAbsoluteNumber(arr) {
+//     if (!Array.isArray(arr) || arr.length <= 0) {
+//         return null;
+//     }
+//     let bigestNum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (bigestNum < Math.abs(arr[i])) {
+//             bigestNum = Math.abs(arr[i])
+//         }
+//     }
+
+//     return bigestNum;
+// };
+
 function getMaxAbsoluteNumber(arr) {
     if (!Array.isArray(arr) || arr.length <= 0) {
         return null;
     }
-    let bigestNum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (bigestNum < Math.abs(arr[i])) {
-            bigestNum = Math.abs(arr[i])
+    let bigestNum = -Infinity;
+
+    arr.forEach(function (num) {
+
+        if (Math.abs(num) > bigestNum) {
+
+            bigestNum = Math.abs(num);
+
         }
-    }
+    })
 
     return bigestNum;
-};
 
-console.log(getMaxAbsoluteNumber([]));
+}
+
+console.log(getMaxAbsoluteNumber(arr));
