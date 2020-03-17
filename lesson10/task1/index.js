785e96cf59918dc8333482e4198b2010a05926a4
Milkweedd <antontab91@gmail.com>
2020-03-17 16:13:19 +0200

    as

--- lesson10/task1/index.js
+++ lesson10/task1/index.js
@@ -1,26 +1,41 @@
-let arr = [1, ' sd', 454, false, true, 78];
+let arr = [1, ' sd', 454, false, true, 78, NaN, null, 0, -0, undefined, Infinity, 21.54, 54, 989.4];
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function getFiniteNumbers(arr) {
     return arr.filter(function (elem) {
-        return Number.isFinite(elem)
+        // console.log(Number.isFinite(elem));
+        return Number.isFinite(elem);
     });
 }
 
-function getFiniteNumbersV2() {
+function getFiniteNumbersV2(arr) {
+    return arr.filter(function (elem) {
+        return isFinite(elem);
+    })
 
 }
 
-function getNaN() {
-
+function getNaN(arr) {
+    return arr.filter(function (elem) {
+        return Number.isNaN(elem);
+    })
 }
 
-function getNaNV2() {
-
+function getNaNV2(arr) {
+    return arr.filter(function (elem) {
+        return isNaN(elem);
+    })
 }
 
-function getIntegers() {
-
+function getIntegers(arr) {
+    return arr.filter(function (elem) {
+        return Number.isInteger(elem);
+    })
 }
 
 
 console.log(getFiniteNumbers(arr));
+console.log(getFiniteNumbersV2(arr));
+console.log(getNaN(arr));
+console.log(getNaNV2(arr));
+console.log(getIntegers(arr));
