95d1b9b37642cbce17d246809d233554990b9a5b
Milkweedd <antontab91@gmail.com>
2020-03-17 23:25:44 +0200

    as

--- lesson10/task5/index.js
+++ lesson10/task5/index.js
@@ -1,9 +1,11 @@
 let arr = [213, 1, 12.12, -412.12, -54, 45, -44.545];
 
 function getTotalPrice(arr) {
-    return arr.reduce(function (acc, num) {
+    let summ = arr.reduce(function (acc, num) {
         return acc + Math.abs(num);
     })
+    console.log(Math.floor(summ * 100) / 100);
+    return Math.floor(summ * 100) / 100;
 }
 
 getTotalPrice(arr);
