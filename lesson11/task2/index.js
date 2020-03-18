6266daad6b4b2fbd27e198522a1020926d205195
Milkweedd <antontab91@gmail.com>
2020-03-18 17:54:06 +0200

    sa

--- lesson11/task2/index.js
+++ lesson11/task2/index.js
@@ -3,15 +3,13 @@ function sortContacts(concats, bollean) {
     if (!Array.isArray(concats)) {
         return null;
     }
-    const result;
     if (bollean === true) {
-        result = concats.sort(function (a, b) {
+        return concats.sort(function (a, b) {
             return a.name.localeCompare(b.name);
         })
     } else {
-        result = concats.sort(function (a, b) {
+        return concats.sort(function (a, b) {
             return b.name.localeCompare(a.name);
         })
     }
-    return result;
 }
