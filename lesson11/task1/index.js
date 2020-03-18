60597eacecf1c24f8518e7e5780c17ff2acf158f
Milkweedd <antontab91@gmail.com>
2020-03-18 17:01:45 +0200

    sa

---
+++ lesson11/task1/index.js
@@ -0,0 +1,27 @@
+function splitText(text, len) {
+    if (typeof (str) !== "string") {
+
+        return null;
+    }
+    let strArr = [];
+    let startPosition = 0;
+    while (true) {
+        let chuck = text.substr(0, len);
+        strArr.push(chuck[0].toUpperCase() + chuck.slice(1));
+
+        startPosition = startPosition + len;
+
+        if (chuck.length === 0) {
+            break;
+        }
+    }
+
+    return strArr.join('\n');
+
+
+}
+
+// console.log(splitText(1452, 444))
+splitText("banani lopala bomba ee , бананы лопала бомба олрайт ", 5);
+
+console.log(splitText("banani lopala bomba ee , бананы лопала бомба олрайт ", 5))
