767589700827021f39d21748c2bca3bcbe4996b7
Milkweedd <antontab91@gmail.com>
2020-03-18 17:17:17 +0200

    sa

--- lesson11/task1/index.js
+++ lesson11/task1/index.js
@@ -1,24 +1,20 @@
-function splitText(text, len) {
+
+
+function splitText(string, len) {
     console.log()
-    if (typeof (text) !== "string") {
+    if (typeof (string) !== 'string') {
         return null;
     }
     let strArr = [];
     let startPosition = 0;
+
     while (true) {
-        let chuck = text.substr(startPosition, len);
-        if (chuck.length === 0) {
-            break;
-        }
-        strArr.push(chuck[0].toUpperCase() + chuck.slice(1));
-
+        let chunk = string.substr(startPosition, len);
+        if (chunk.length === 0) break;
+        strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
         startPosition = startPosition + len;
-
     }
-
     return strArr.join('\n');
-
-
 }
 
 // console.log(splitText(1452, 444))
