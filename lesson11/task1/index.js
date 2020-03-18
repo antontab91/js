05d7cb58763215a566a6f26cd380fc462bc3b9b9
Milkweedd <antontab91@gmail.com>
2020-03-18 17:27:43 +0200

    a

--- lesson11/task1/index.js
+++ lesson11/task1/index.js
@@ -1,5 +1,5 @@
 function splitText(string, len = 10) {
-    sa
+    console.log()
     if (typeof (string) !== 'string') {
         return null;
     }
@@ -18,4 +18,4 @@ function splitText(string, len = 10) {
 // console.log(splitText(1452, 444))
 // splitText("banani lopala bomba ee , бананы лопала бомба олрайт ", 5);
 
-console.log(splitText("sa5assasa", 5))
+// console.log(splitText("sa5assasa", 5))
