5a988ff9e1c0f48089458d2c5d644ca71cf8fa8b
Milkweedd <antontab91@gmail.com>
2020-03-22 01:27:44 +0200

    as

---
+++ lesson13/task1/index.js
@@ -0,0 +1,8 @@
+
+// function getEvenNumbers(arr) {
+//     return arr.filter(function (num) {
+//         return num % 2 === 0;
+//     });
+// }
+
+
---
+++ lesson13/task1/index.test.js
@@ -0,0 +1,19 @@
+it('17 и в Африке 17', () => {
+    expect(17).toEqual(17);
+});
+
+it('18 это вам не 17', () => {
+    expect(18).not.toEqual(17);
+});
+
+function getEvenNumbers(arr) {
+    return arr.filter(function (num) {
+        return num % 2 === 0;
+    });
+}
+
+
+it('should get only even nubers from array', () => {
+    const result = getEvenNumbers([1, 2, 3, 4]);
+    expect(result).toEqual([2, 4]);
+});
