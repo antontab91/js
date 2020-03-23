b8c0426c494e63d89ad04de9eab4182f94e9737c
Milkweedd <antontab91@gmail.com>
2020-03-23 12:35:44 +0200

    sa

--- lesson15/task3/index.js
+++ lesson15/task3/index.js
@@ -1,32 +1,26 @@
 export function createLogger() {
     let memory = [];
 
-    function warn(warning) {
-        return memory.push({ message: warning, dateTime: new Date(), type: 'warn' });
+    const warn = (warning) => {
+        return memory.push({ message: warning, dateTime: new Date(), type: 'warn', });
     }
 
-    function error(err) {
-        return memory.push({ message: err, dateTime: new Date(), type: 'error' });
+    const error = (err) => {
+        return memory.push({ message: err, dateTime: new Date(), type: 'error', });
     }
 
-    function log(log) {
-        return memory.push({ message: log, dateTime: new Date(), type: 'log' });
+    const log = (log) => {
+        return memory.push({ message: log, dateTime: new Date(), type: 'log', })
     }
 
-    function getRecords(type) {
-        if (!type) {
-            return memory;
-        }
+    const getRecords = (type) => {
+        if (!type) { return memory };
         return memory.filter((a) => a.type === type).sort((a, b) => a.dateTime - b.dateTime);
     }
-
     return {
         warn,
         error,
         log,
         getRecords,
-    };
+    }
 }
-
-// const proverka = createLogger();
-// console.log(proverka.getRecords())
