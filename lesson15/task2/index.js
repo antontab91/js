cd7550ca3bfd3fc09c5217b437e71601de8e129e
Milkweedd <antontab91@gmail.com>
2020-03-23 10:10:59 +0200

    sa

---
+++ lesson15/task2/index.js
@@ -0,0 +1,27 @@
+export function createCalculator(num) {
+    let memory = 0;
+    function add(num) {
+        memory = memory + num;
+        return memory;
+    }
+
+    function decrease(num) {
+        memory = memory - num;
+        return memory;
+    }
+    function reset(num) {
+        memory = 0
+        return memory;
+    }
+    function getMemo(num) {
+        return memory;
+    }
+
+    return {
+        add,
+        decrease,
+        reset,
+        getMemo
+    };
+}
+
