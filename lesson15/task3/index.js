894cbea58e95ef3d12dafd1e72e6f551cd39cac2
Milkweedd <antontab91@gmail.com>
2020-03-23 12:32:58 +0200

    as

--- lesson15/task3/index.js
+++ lesson15/task3/index.js
@@ -1,28 +1,32 @@
-export function creatememory() {
+export function createLogger() {
     let memory = [];
 
     function warn(warning) {
-        return memory.push({ message: warning, dateTime: new Date(), type: 'warn', });
+        return memory.push({ message: warning, dateTime: new Date(), type: 'warn' });
     }
 
-    function error(error) {
-        return memory.push({ message: error, dateTime: new Date(), type: 'error', });
+    function error(err) {
+        return memory.push({ message: err, dateTime: new Date(), type: 'error' });
     }
 
     function log(log) {
-        return memory.push({ message: log, dateTime: new Date(), type: 'log', })
+        return memory.push({ message: log, dateTime: new Date(), type: 'log' });
     }
 
     function getRecords(type) {
         if (!type) {
             return memory;
-        };
+        }
         return memory.filter((a) => a.type === type).sort((a, b) => a.dateTime - b.dateTime);
     }
+
     return {
         warn,
         error,
         log,
-        getRecords
+        getRecords,
     };
 }
+
+// const proverka = createLogger();
+// console.log(proverka.getRecords())
