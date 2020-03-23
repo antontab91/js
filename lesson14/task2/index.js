8031e5532775d10a6c7f645f179c5688e4c1db2a
Milkweedd <antontab91@gmail.com>
2020-03-23 02:44:04 +0200

    sa

---
+++ lesson14/task2/index.js
@@ -0,0 +1,23 @@
+export const createMessenger = () => {
+    let message = 'Just learn it';
+    const sender = 'Gromcode';
+
+    function sendMessage(name) {
+
+        console.log(`${name}, ${message}! Your ${sender}`);
+    }
+
+    function setMessage(text) {
+        message = text;
+    }
+
+    function setSender(senderText) {
+        sender = senderText;
+    }
+
+    return {
+        sendMessage,
+        setMessage,
+        setSender
+    }
+}
