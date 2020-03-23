947c6c07c48aeed87a81a2291c7d2de4fb040ac4
Milkweedd <antontab91@gmail.com>
2020-03-23 02:55:45 +0200

    sa

---
+++ lesson14/task3/index.js
@@ -0,0 +1,16 @@
+let memory = 0;
+
+export function add(num) {
+    return memory + num;
+}
+
+export function decrease(num) {
+    return memory - num;
+}
+export function reset() {
+    memory = 0;
+    return memory;
+}
+export function getMemo(num) {
+    return num;
+}
