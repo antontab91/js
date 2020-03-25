cf08c90df1b27b4fa529ab463b3563e8d257a52f
Milkweedd <antontab91@gmail.com>
2020-03-25 03:59:07 +0200

    as

--- lesson16/task3/index.js
+++ lesson16/task3/index.js
@@ -1,4 +1,8 @@
 export function createArrayOfFunctions(num) {
+    if (typeof num !== 'number') {
+        return null;
+    }
+
     let arr = [];
     for (let i = 0; i < num; i++) {
         arr[i] = function () {
