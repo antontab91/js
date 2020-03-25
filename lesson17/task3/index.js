f95ac60da18d5f23fc2dd23c781d0fcfcd8ff9fc
Milkweedd <antontab91@gmail.com>
2020-03-25 18:45:39 +0200

    sa

--- lesson17/task3/index.js
+++ lesson17/task3/index.js
@@ -1,6 +1,6 @@
 export function defer(func, ms) {
     return function (ms) {
-        setTimeout(() => func(...arguments), ms);
+        setTimeout(() => func(...arguments).bind(this), ms);
 
     };
 }
