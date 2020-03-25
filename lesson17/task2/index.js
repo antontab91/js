26668c5f5b170f69372b07f9b788be056af22fcb
Milkweedd <antontab91@gmail.com>
2020-03-25 17:14:05 +0200

    as

---
+++ lesson17/task2/index.js
@@ -0,0 +1,13 @@
+export const callbackPrompt = {
+    message: 'Tell me your number',
+    showPrompt() {
+        const phoneNumber = prompt(this.message);   // чтобы использовать данные , которые ввел пользователь нужно промпт присвоить переменной 
+        console.log(phoneNumber);
+    },
+
+    showDeferredPrompt(ms) {
+        setTimeout(this.showPrompt, ms);
+    }
+}
+
+callbackPrompt.showDeferredPrompt(4000);
