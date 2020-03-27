73b80585bfa4147741fabf950a4af310c08e066f
Milkweedd <antontab91@gmail.com>
2020-03-27 02:49:54 +0200

    sa

---
+++ lesson18/task2/index.js
@@ -0,0 +1,9 @@
+const wallet = {
+    transactions: [1, 5, 89, 337, 3],
+    getMax() {
+        return Math.max(...this.transactions);
+    },
+    getMin() {
+        return Math.min(...this.transactions);
+    }
+};
--- lesson18/task4/index.js
+++
@@ -1,9 +0,0 @@
-export function spy(func) {
-    getWrapper.calls = [];
-
-    function getWrapper(...args) {
-        getWrapper.calls.push(args);
-        return func.apply(this, args);
-    }
-    return getWrapper;
-};
