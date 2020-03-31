3392c2283914571db5a220fddb284fef8b2ad019
Milkweedd <antontab91@gmail.com>
2020-03-31 14:02:29 +0300

    as

--- lesson17/task6/index.js
+++ lesson17/task6/index.js
@@ -1,13 +1,13 @@
-// export function bind(fn, context) {
+export function bind(fn, context) {
 
-//     let bindArgs = [].slice.call(arguments, 2);
-//     return function () {
+    let bindArgs = [].slice.call(arguments, 2);
+    return function () {
 
-//         let fnArgs = [].slice.call(arguments);
+        let fnArgs = [].slice.call(arguments);
 
-//         return fn.apply(context, bindArgs.concat(fnArgs));
-//     };
-// };
+        return fn.apply(context, bindArgs.concat(fnArgs));
+    };
+};
 
 // export function bind(fn, context) {
 //     return function (...arg) {
@@ -15,12 +15,12 @@
 //     };
 // }
 
-export function bind(context, func, ...xz) {
-    console.log(...xz)
-    return function (...arg) {
-        console.log(...arg)
-        return func.apply(context, [...xz, ...arg]);
-    };
-}
+// export function bind(context, func, ...xz) {
+//     console.log(...xz)
+//     return function (...arg) {
+//         console.log(...arg)
+//         return func.apply(context, [...xz, ...arg]);
+//     };
+// }
 
 // http://jsraccoon.ru/interview-bind  - в этой статье теория о том как написать эту функцию , которая мне помогла . 
