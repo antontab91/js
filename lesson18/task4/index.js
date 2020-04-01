2a218e4aab91b5cab26740abbc7fbf4b08dab94e
Milkweedd <antontab91@gmail.com>
2020-04-01 19:46:05 +0300

    as

--- lesson18/task4/index.js
+++ lesson18/task4/index.js
@@ -10,3 +10,30 @@ export function saveCalls(allCalls) {
 
 }
 
+
+
+    // function test(a, b) {
+    //     return Math.sqrt(a * a + b * b);
+    // }
+
+    // const testWithMemory = saveCalls(test);
+    // testWithMemory(4, 2);
+    // testWithMemory(9, 1);
+
+
+    // testWithMemory.calls // [ [4, 2], [9, 1] ]
+
+    // /* Поддержка функций с контекстом */
+
+    // const user = {
+    //     name: 'John',
+    //     sayHi() {
+    //         return this.name;
+    //     }
+    // };
+
+    // const methodWithMemory = saveCalls(user.sayHi);
+
+    // methodWithMemory.apply({ name: 'Tom' }); // 'Tom'
+
+    // methodWithMemory.calls // [ [] ]
---
+++ lesson19/task1/index.js
@@ -0,0 +1,33 @@
+export const vehicle = {
+    name: 'Argo',
+    move() {
+        console.log(`${this.name} is moving`);
+    },
+    stop() {
+        console.log(`${this.name} is moving`);
+    }
+};
+
+export const ship = {
+    // name: 'Aurora',
+    // hasWeels: false,
+    // liftAnchorDown() {
+    //     console.log('lifting anchor down');
+    // },
+    // liftAnchorUp() {
+    //     console.log('lifting anchor up');
+    // },
+
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`);
+        this.move();
+    },
+    stopMachine() {
+        this.stop();
+        console.log(`${this.name} lifting anchor down`);
+    }
+
+};
+
+Object.setPrototypeOf(ship, vehicle);
+
