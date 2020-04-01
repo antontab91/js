affa72dac05ad8924bffee45455dfee930f3c3f6
Milkweedd <antontab91@gmail.com>
2020-04-01 21:24:18 +0300

    as

---
+++ lesson19/task4/index.js
@@ -0,0 +1,20 @@
+export function User(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+User.prototype.sayHi = function () {
+    console.log(`Hi, I am ${this.name}`);
+}
+
+User.prototype.requestNewPhoto = function () {
+    console.log(`New photo request was sent for ${this.name}`);
+}
+
+User.prototype.setAge = function () {
+    this.age = age;
+    if (this.age >= 25) {
+        console.log(`New photo request was sent for ${this.name}`);
+    }
+}
+
