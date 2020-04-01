3b482013b1ecbc45ff25ede7175767cc1103e70e
Milkweedd <antontab91@gmail.com>
2020-04-01 20:37:26 +0300

    as

--- lesson19/task2/index.js
+++ lesson19/task2/index.js
@@ -1,9 +1,20 @@
+export const obj1 = {
+    name: "saas",
+    bubu() {
+        console.log(this.name)
+    }
+
+}
+
 export function getOwnProps(obj) {
     let arr = [];
     for (let prop in obj) {
-        if ((typeof obj.hasOwnProperty(prop)) !== 'function') {
+        if (typeof obj[prop] !== 'function' && obj.hasOwnProperty(prop)) {
             arr.push(prop);
         }
     }
     return arr;
 }
+
+
+console.log(getOwnProps(obj1));
