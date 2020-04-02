33bdaf8e01cc7efbee7081ccaea79883dc577c95
Milkweedd <antontab91@gmail.com>
2020-04-02 17:34:07 +0300

    sa

--- lesson20/task1/index.js
+++ lesson20/task1/index.js
@@ -1,20 +1,20 @@
 
-// export function User2(name, age) {                   // конструктор функция *( по старому ) и конструктор внутри клсса выполняют одни и те же задачи
-//     this.name = name;
-//     this.age = age;
-// }
-// User2.prototype.sayHi = function () {
-//     console.log(`Hi, I am ${this.name}`);
-// }
-// User2.prototype.requestNewPhoto = function () {
-//     console.log(`New photo request was sent for ${this.name}`);
-// };
-// User2.prototype.setAge = function (age) {
-//     if (age < 0) { return false; }
-//     this.age = age;
-//     if (age >= 25) { this.requestNewPhoto() }
-//     return age;
-// };
+export function User2(name, age) {                   // конструктор функция *( по старому ) и конструктор внутри клсса выполняют одни и те же задачи
+    this.name = name;
+    this.age = age;
+}
+User2.prototype.sayHi = function () {
+    console.log(`Hi, I am ${this.name}`);
+}
+User2.prototype.requestNewPhoto = function () {
+    console.log(`New photo request was sent for ${this.name}`);
+};
+User2.prototype.setAge = function (age) {
+    if (age < 0) { return false; }
+    this.age = age;
+    if (age >= 25) { this.requestNewPhoto() }
+    return age;
+};
 
 
 export class User {
@@ -22,6 +22,11 @@ export class User {
         this.name = name;
         this.age = age;
     }
+
+    static createEmpty() {
+        return new User('', null);
+    }
+
     sayHi() {
         console.log(`Hi, I am ${this.name}`);
     }
@@ -36,25 +41,25 @@ export class User {
     }
 }
 
-// const user2 = new User2('Miska-Taptiska', 21);
-// console.log(user2);
-// console.log(user2.sayHi());
-// console.log(user2.setAge(15));
-// console.log(user2);
-// console.log(user2.setAge(1781));
-// console.log(user2);
+const user2 = new User2('Miska-Taptiska', 21);
+console.log(user2);
+console.log(user2.sayHi());
+console.log(user2.setAge(15));
+console.log(user2);
+console.log(user2.setAge(1781));
+console.log(user2);
 
 
-// const user = new User('Mokshan', 54);
-// console.log(user);
-// console.log(user.sayHi());
-// console.log(user.setAge(105));
-// console.log(user);
-// console.log(user.setAge(11));
-// console.log(user);
+const user = new User('Mokshan', 54);
+console.log(user);
+console.log(user.sayHi());
+console.log(user.setAge(105));
+console.log(user);
+console.log(user.setAge(11));
+console.log(user);
 
 
 
-// console.log(user);
-// console.log(user2);
+console.log(user);
+console.log(user2);
 
