13a849d958a739e32d6584a16085a5e6ba574bc2
Milkweedd <antontab91@gmail.com>
2020-04-02 18:14:27 +0300

    sa

---
+++ lesson20/task3/index.js
@@ -0,0 +1,16 @@
+export class Wallet {
+    constructor() {
+        this._balance = 0;
+    }
+    getBalance() { return this._balance }
+    deposit(amount) {
+        this._balance += amount;
+    }
+    withdraw(amount) {
+        if (amount > this._balance) {
+            console.log(`No enough funds`);
+            return;
+        }
+        return this._balance -= amount;
+    }
+}
