2fb4773236378570f6cc11d6a45e0208fb827137
Milkweedd <antontab91@gmail.com>
2020-04-06 10:10:05 +0300

    dsa

---
+++ lesson21/task6/index.js
@@ -0,0 +1,19 @@
+const tasks = [
+    { text: 'Buy milk', done: false },
+    { text: 'Pick up Tom from airport', done: false },
+    { text: 'Visit party', done: false },
+    { text: 'Visit doctor', done: true },
+    { text: 'Buy meat', done: true },
+];
+
+// let ulList = document.querySelector('.list');
+// let listItem = document.createElement('li');
+// let listItemCheckbox = document.createElement('input');
+
+
+// listItem.className = 'list__item';
+// listItemCheckbox.setAttribute('type', 'checkbox');
+// listItemCheckbox.className = 'list__item-checkbox';
+
+// ulList.append(listItem);
+// listItem.append(listItemCheckbox);
