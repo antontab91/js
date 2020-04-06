aa68ae97260c010591965e0db5dea7a676d5829a
Milkweedd <antontab91@gmail.com>
2020-04-06 23:44:31 +0300

    sa

--- lesson21/task6/index.js
+++ lesson21/task6/index.js
@@ -1,19 +1,6 @@
-const tasks = [
-    { text: 'Buy milk', done: false },
-    { text: 'Pick up Tom from airport', done: false },
-    { text: 'Visit party', done: false },
-    { text: 'Visit doctor', done: true },
-    { text: 'Buy meat', done: true },
-];
-
-// let ulList = document.querySelector('.list');
-// let listItem = document.createElement('li');
-// let listItemCheckbox = document.createElement('input');
+export function setButton(buttonText) {
+    document.querySelector('body').innerHTML = `<button>${buttonText}</button > `;
+}
 
 
-// listItem.className = 'list__item';
-// listItemCheckbox.setAttribute('type', 'checkbox');
-// listItemCheckbox.className = 'list__item-checkbox';
-
-// ulList.append(listItem);
-// listItem.append(listItemCheckbox);
+// setButton("sdadas");
