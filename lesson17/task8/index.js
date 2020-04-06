950e1c4f71d4a244510ec9702c1f1c6b6fe2f575
Milkweedd <antontab91@gmail.com>
2020-04-06 15:02:37 +0300

    sa

--- lesson17/task8/index.js
+++ lesson17/task8/index.js
@@ -1,26 +1,81 @@
-export function bind(fn, context) {
-
-    let bindArgs = [].slice.call(arguments, 2);
-    return function () {
-
-        let fnArgs = [].slice.call(arguments);
-
-        return fn.apply(context, bindArgs.concat(fnArgs));
-    };
+/* ===> 1 <=== */
+const student = {
+    name: 'Tom',
 };
 
-// export function bind(fn, context) {
-//     return function (...arg) {
-//         return fn.apply(context, [...arg]);
-//     };
-// }
+const student2 = {
+    name: 'Bruce',
+};
 
-// export function bind(context, func, ...xz) {
-//     console.log(...xz)
-//     return function (...arg) {
-//         console.log(...arg)
-//         return func.apply(context, [...xz, ...arg]);
-//     };
-// }
+export function sayName() {
+    console.log(this.name);
+}
 
-// http://jsraccoon.ru/interview-bind  - в этой статье теория о том как написать эту функцию , которая мне помогла . 
+// вызовите ф-цию sayName так, чтобы в консоль вывелось имя студента 
+// ... your code here
+sayName.apply(student);
+
+// вызовите ф-цию sayName так, чтобы в консоль вывелось имя 'Bruce' (используйте другой объект)
+// ... your code here
+sayName.apply(student2);
+
+
+/* ===> 2 <=== */
+const company = {
+    companyName: 'Microsoft'
+};
+
+function greeting(firstName, lastName) {
+    console.log(`Hello, ${firstName} ${lastName}. Welcome to the ${this.companyName}`);
+}
+
+// вызовите ф-цию greeting так, чтобы в консоль вывелось 
+// 'Hello, Bob Marley. Welcome to the Microsoft'
+// используйте объект company
+// ... your code here
+
+greeting.apply(company, ['Bob', 'Marley']);
+
+
+/* ===> 3 <=== */
+const country = {
+    countryName: 'Ukraine',
+    capital: 'Kyiv'
+};
+
+function getPopulation(population) {
+    console.log(`Population in ${this.countryName} is ${population}`);
+    return `Population in ${this.countryName} is ${population}`;
+}
+
+// вызовите ф-цию getPopulation так, чтобы она вернула 
+// 'Population in Ukraine is 43000'
+// используйте объект country
+// ... your code here
+getPopulation.apply(country, ['43000']);
+
+
+/* ===> 4 <=== */
+const transaction = {
+    amount: 1200,
+    operation: 'sell',
+    currency: 'USD',
+    exchange: 'NYSE',
+    printTransaction() {
+        console.log(`${this.amount} ${this.currency} - ${this.operation} on ${this.exchange}`);
+    }
+}
+
+const anotherTransaction = {
+    amount: 400,
+    operation: 'buy',
+    currency: 'USD',
+    exchange: 'NASDAQ',
+};
+
+// вызовите метод transaction.printTransaction так, чтобы в консоль вывелось
+// '400 USD - buy on NASDAQ'
+// используйте объект anotherTransaction как контекст
+// ... your code here
+
+transaction.printTransaction.apply(anotherTransaction);
