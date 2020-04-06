847893e6c105e5ecf97f5a98c75fe105106de0ff
Milkweedd <antontab91@gmail.com>
2020-04-06 21:37:08 +0300

    as

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -34,23 +34,26 @@
 
 
 
+// getTitle();
+// getDescription();
+// getPlans();
+// getGoal();
+
+
+
 
 export function getTitle() {
     return document.querySelector('.title').textContent;
 };
-getTitle();
 
 export function getDescription() {
     return document.querySelector('.about').innerText;
 };
-getDescription();
 
 export function getPlans() {
     return document.querySelector('.plans').innerHTML;
 }
-getPlans();
 
 export function getGoal() {
     return document.querySelector('.goal').outerHTML;
 };
-getGoal();
