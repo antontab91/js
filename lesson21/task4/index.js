4ab05523b4e8be6974c6935729226e565d5e771f
Milkweedd <antontab91@gmail.com>
2020-04-06 21:34:20 +0300

    sa

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -1,56 +1,56 @@
-export function getTitle() {
-    let titleText = document.querySelector('.title').textContent;
-    // console.dir(titleText);
-    // console.log(titleText);
-    return titleText;
-}
-
-
-
-export function getDescription() {
-    let aboutText = document.querySelector('.about').innerText;
-    // console.dir(aboutText);
-    // console.log(aboutText);
-    return aboutText;
-}
-
-
-
-export function getPlans() {
-    let plansText = document.querySelector('.plans').innerHTML;
-    // console.dir(plansText);
-    // console.log(plansText);
-    return plansText;
-}
-
-
-
-export function getGoal() {
-    let goalText = document.querySelector('.goal').outerHTML;
-    // console.dir(goalText);
-    // console.log(goalText);
-    return goalText;
-}
-
-
-
-
 // export function getTitle() {
-//     return document.querySelector('.title').textContent;
-// };
-getTitle();
+//     let titleText = document.querySelector('.title').textContent;
+//     console.dir(titleText);
+//     console.log(titleText);
+//     return titleText;
+// }
+
+
 
 // export function getDescription() {
-//     return document.querySelector('.about').innerText;
-// };
-getDescription();
+//     let aboutText = document.querySelector('.about').innerText;
+//     console.dir(aboutText);
+//     console.log(aboutText);
+//     return aboutText;
+// }
+
+
 
 // export function getPlans() {
-//     return document.querySelector('.plans').innerHTML;
+//     let plansText = document.querySelector('.plans').innerHTML;
+//     console.dir(plansText);
+//     console.log(plansText);
+//     return plansText;
 // }
-getPlans();
+
+
 
 // export function getGoal() {
-//     return document.querySelector('.goal').outerHTML;
-// };
+//     let goalText = document.querySelector('.goal').outerHTML;
+//     console.dir(goalText);
+//     console.log(goalText);
+//     return goalText;
+// }
+
+
+
+
+export function getTitle() {
+    return document.querySelector('.title').textContent;
+};
+getTitle();
+
+export function getDescription() {
+    return document.querySelector('.about').innerText;
+};
+getDescription();
+
+export function getPlans() {
+    return document.querySelector('.plans').innerHTML;
+}
+getPlans();
+
+export function getGoal() {
+    return document.querySelector('.goal').outerHTML;
+};
 getGoal();
