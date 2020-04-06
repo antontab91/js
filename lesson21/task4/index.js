f239acbdb2284ae50fe0c3b06079765c07454a61
Milkweedd <antontab91@gmail.com>
2020-04-06 21:33:30 +0300

    sa

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -1,35 +1,56 @@
 export function getTitle() {
     let titleText = document.querySelector('.title').textContent;
-    console.dir(titleText);
-    console.log(titleText);
+    // console.dir(titleText);
+    // console.log(titleText);
     return titleText;
-};
+}
 
-getTitle()
+
 
 export function getDescription() {
     let aboutText = document.querySelector('.about').innerText;
-    console.dir(aboutText);
-    console.log(aboutText);
+    // console.dir(aboutText);
+    // console.log(aboutText);
     return aboutText;
 }
 
-getDescription();
+
 
 export function getPlans() {
     let plansText = document.querySelector('.plans').innerHTML;
-    console.dir(plansText);
-    console.log(plansText);
+    // console.dir(plansText);
+    // console.log(plansText);
     return plansText;
 }
 
-getPlans()
+
 
 export function getGoal() {
     let goalText = document.querySelector('.goal').outerHTML;
-    console.dir(goalText);
-    console.log(goalText);
+    // console.dir(goalText);
+    // console.log(goalText);
     return goalText;
 }
 
-getGoal() 
+
+
+
+// export function getTitle() {
+//     return document.querySelector('.title').textContent;
+// };
+getTitle();
+
+// export function getDescription() {
+//     return document.querySelector('.about').innerText;
+// };
+getDescription();
+
+// export function getPlans() {
+//     return document.querySelector('.plans').innerHTML;
+// }
+getPlans();
+
+// export function getGoal() {
+//     return document.querySelector('.goal').outerHTML;
+// };
+getGoal();
