1687dd24bbee8a9d923c046f0e94f461b48ef045
Milkweedd <antontab91@gmail.com>
2020-04-07 16:42:59 +0300

    as

--- lesson22/task1/index.js
+++ lesson22/task1/index.js
@@ -1,4 +1,4 @@
-const task = [
+const tasks = [
     { text: 'Buy milk', done: false },
     { text: 'Pick up Tom from airport', done: false },
     { text: 'Visit party', done: false },
@@ -6,31 +6,27 @@ const task = [
     { text: 'Buy meat', done: true },
 ];
 
-function renderListItems(listItems) {
+const renderListItems = listItems => {
     const listElem = document.querySelector('.list');
 
-    const listTasks = listItems.sort((a, b) => a.done - b.done)
+    const listItemsElems = listItems
+        .sort((a, b) => a.done - b.done)
         .map(({ text, done }) => {
             const listItemElem = document.createElement('li');
-            const checkboxElem = document.createElement('input')
-            checkboxElem.classList.add('list__checkbox');
-            checkboxElem.setAttribute('type', 'checkbox');
             listItemElem.classList.add('list__item');
-            if (done) listItemElem.classList.add('list__item_checked');
+            if (done) {
+                listItemElem.classList.add('list__item_done');
+            }
+            const checkboxElem = document.createElement('input');
+            checkboxElem.setAttribute('type', 'checkbox');
             checkboxElem.checked = done;
+            checkboxElem.classList.add('list__item-checkbox');
+
             listItemElem.append(checkboxElem, text);
 
             return listItemElem;
         });
-
-
-    listElem.append(...listTasks);
-
-
-    return listTasks;
-
-
+    listElem.append(...listItemsElems);
 };
 
-
-renderListItems(task);
+renderListItems(tasks);
