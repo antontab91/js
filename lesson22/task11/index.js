21bb612c6d5678cdeab76bb39ba54e02f118cef5
Milkweedd <antontab91@gmail.com>
2020-04-07 17:07:07 +0300

    as

--- lesson22/task11/index.js
+++ lesson22/task11/index.js
@@ -1,69 +1,80 @@
 const tasks = [
-    { text: 'Buy milk', done: false, date: new Date(), id: getRandomId() },
-    { text: 'Pick up Tom from airport', done: false, date: new Date(), id: getRandomId() },
-    { text: 'Visit party', done: false, date: new Date(), id: getRandomId() },
-    { text: 'Visit doctor', done: true, date: new Date(), id: getRandomId() },
-    { text: 'Buy meat', done: true, date: new Date(), id: getRandomId() },
+    {
+        text: "Buy milk",
+        done: false
+    },
+    {
+        text: "Pick up Tom from airport",
+        done: false
+    },
+    {
+        text: "Visit party",
+        done: false
+    },
+    {
+        text: "Visit doctor",
+        done: true
+    },
+    {
+        text: "Buy meat",
+        done: true
+    }
 ];
 
+const renderListItem = listItems => {
+    const listElement = document.querySelector(".list");
+    listElement.innerHTML = "";
 
-function getRandomId() {
-    return Math.floor(Math.random() * 10000);
-}
-
-
-
-const renderListItems = listItems => {
-    const list = document.querySelector('.list');
-    list.innerHTML = '';
-
-    const listElements = listItems
+    const listItemsElements = listItems
         .sort((a, b) => a.done - b.done)
-        .map(({ text, done, id = getRandomId() }) => {
-            const listElement = document.createElement('li');
-            listElement.classList.add('list__item');
+        .map(({ text, done }) => {
+            const listItemElement = document.createElement("li");
+            listItemElement.classList.add("list__item");
             if (done) {
-                listElement.classList.add('list__item_done');
+                listItemElement.classList.add("list__item_done");
             }
-            const checkboxElement = document.createElement('input');
-            checkboxElement.setAttribute('type', 'checkbox');
-            checkboxElement.setAttribute('data-id', id);
+            const checkboxElement = document.createElement("input");
+            checkboxElement.setAttribute("type", "checkbox");
             checkboxElement.checked = done;
-            checkboxElement.classList.add('list__item-checkbox');
-            listElement.append(checkboxElement, text);
+            checkboxElement.classList.add("list__item-checkbox");
+            listItemElement.append(checkboxElement, text);
 
-            return listElement;
+            return listItemElement;
         });
+    listElement.append(...listItemsElements);
+};
 
-    list.append(...listElements);
-}
-renderListItems(tasks);
+const getValueInput = document.querySelector(".task-input");
+// console.log(getValueInput.value)
 
+const btn = document.querySelector(".create-task-btn");
 
+function createNewTask() {
+    tasks.push({
+        text: getValueInput.value,
+        done: false
+    });
 
-const listWithTasks = document.querySelector('.list');
-const updateTasksList = event => {
-    const isCheckbox = event.target.classList.contains('list__item-checkbox');
-    if (!isCheckbox) {
-        return;
-    }
-    // console.log(tasks.map(item => item.id));
-    const taskData = tasks.find(task => task.id == event.target.dataset.id);
-    Object.assign(taskData, { done: event.target.checked });
-    renderListItems(tasks);
-}
-const updateTasksListHandler = listWithTasks.addEventListener('click', updateTasksList);
+    if (getValueInput.value === "") return false;
+    getValueInput.value = "";
 
+    return renderListItem(tasks);
+};
 
+btn.addEventListener("click", createNewTask);
 
-const creteBtn = document.querySelector('.create-btn');
-const addNewTask = () => {
-    const input = document.querySelector('.task-input');
+const choiceList = document.querySelector(".list");
 
-    if (!input.value) return;
-    tasks.unshift({ text: `${input.value}`, done: false, date: new Date(), id: getRandomId() });
-    input.value = '';
+function check(e) {
+    const similar = tasks.find(item => item.text === e.target.parentNode.textContent);
+    console.log(e.target.parentNode.textContent);
+    similar.done = e.target.checked;
+    console.log(e.target.checked);
+    console.log(similar.done);
 
-    renderListItems(tasks);
-}
-const addNewTaskHandler = creteBtn.addEventListener('click', addNewTask);
+    renderListItem(tasks);
+};
+
+choiceList.addEventListener("click", check);
+
+renderListItem(tasks);
