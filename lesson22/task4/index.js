b38d17c423ff631f156f1532d43695e7843ff941
Milkweedd <antontab91@gmail.com>
2020-04-07 15:23:53 +0300

    as

---
+++ lesson22/task4/index.js
@@ -0,0 +1,7 @@
+document.querySelector('.task-status').addEventListener('change', function () {
+    // if (document.querySelector('.task-status').checked === true) {
+    //     console.log(true);
+    // }
+
+    console.log(document.querySelector('.task-status').checked);
+});
