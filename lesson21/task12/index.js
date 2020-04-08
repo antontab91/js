6064856ed08fd346de4fcff222d30d3e652ea13d
Milkweedd <antontab91@gmail.com>
2020-04-08 23:35:13 +0300

    as

--- lesson21/task12/index.js
+++ lesson21/task12/index.js
@@ -13,6 +13,6 @@
 
 export const squaredNumbers = () => Array
     .from(document.querySelectorAll('.number'))
-    .forEach(elem => elem.setAttribute('data-squared-number', Math.pow(elem.dataset.number, 2)));
+    .forEach(el => el.setAttribute('data-squared-number', Math.pow(el.dataset.number, 2)));
 
 // squaredNumbers()
