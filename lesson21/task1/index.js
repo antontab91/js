c0c59deef85e8acdf8a7adbff56121ddb3f4d851
Milkweedd <antontab91@gmail.com>
2020-04-08 16:23:22 +0300

    sa

--- lesson21/task1/index.js
+++ lesson21/task1/index.js
@@ -5,23 +5,32 @@ const tasks = [
     { text: 'Visit doctor', done: true },
     { text: 'Buy meat', done: true },
 ];
-const getListItems = listItems => {
-    const listElem = document.querySelector('.list');
-    const listItemElems = listItems.sort((a, b) => a.done - b.done)
-        .map(({ text, done }) => {
-            const listItemElem = document.createElement('li');
-            listItemElem.classList.add('list__item');
-            if (done) {
-                listItemElem.classList.add('list__item_done');
-            }
-            const checkboxElem = document.createElement('input');
-            checkboxElem.setAttribute('type', 'checkbox');
-            checkboxElem.checked = done;
-            checkboxElem.classList.add('list__item-checkbox');
-            listItemElem.append(checkboxElem, text);
-            return listItemElem;
 
-        });
-    listElem.append(...listItemElems);
-};
-getListItems(tasks);
+const list = document.querySelector('.list');
+let sortArr = [];
+
+function sortTasks(arr) {
+    sortArr = arr.sort(function (a, b) {
+        return a.done - b.done;
+    });
+    return sortArr;
+}
+
+function pushTasks(arr) {
+    return arr.map(function (elem) {
+        let listItem = document.createElement('li');
+        listItem.classList.add('list__item')
+        if (elem.done) {
+            listItem.className = 'list__item list__item_done';
+        }
+        listItem.innerHTML = elem.text;
+        list.append(listItem);
+        console.log('2');
+    })
+}
+
+
+sortTasks(tasks);
+pushTasks(sortArr);
+
+console.log(sortArr);
