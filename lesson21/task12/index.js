62f01329e6f181f97971e401599f8be86d33f15e
Milkweedd <antontab91@gmail.com>
2020-04-08 23:35:02 +0300

    as

--- lesson21/task12/index.js
+++ lesson21/task12/index.js
@@ -1,18 +1,18 @@
-export function squaredNumbers() {
-    let arr = [...document.querySelectorAll('.number')];
-    arr.forEach(function (el) {
-        let attrValue = el.getAttribute('data-number');
-        // console.log(arr);
-        // console.log(attrValue)
-        // console.log(typeof attrValue)
-        // console.log(Math.pow(attrValue, 2));
-        el.setAttribute('data-squared-number', Math.pow(attrValue, 2));
-    });
-}
+// export function squaredNumbers() {
+//     let arr = [...document.querySelectorAll('.number')];
+//     arr.forEach(function (el) {
+//         let attrValue = el.getAttribute('data-number');
+//         // console.log(arr);
+//         // console.log(attrValue)
+//         // console.log(typeof attrValue)
+//         // console.log(Math.pow(attrValue, 2));
+//         el.setAttribute('data-squared-number', Math.pow(attrValue, 2));
+//     });
+// }
 
 
-// export const squaredNumbers = () => Array
-//     .from(document.querySelectorAll('.number'))
-//     .forEach(elem => elem.setAttribute('data-squared-number', Math.pow(elem.dataset.number, 2)));
+export const squaredNumbers = () => Array
+    .from(document.querySelectorAll('.number'))
+    .forEach(elem => elem.setAttribute('data-squared-number', Math.pow(elem.dataset.number, 2)));
 
 // squaredNumbers()
