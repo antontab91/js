cb6530c966e280ffc4dd77a1b439c9409c0cff08
Milkweedd <antontab91@gmail.com>
2020-04-08 16:54:10 +0300

    sa

--- lesson21/task1/index.js
+++ lesson21/task1/index.js
@@ -16,21 +16,54 @@ function sortTasks(arr) {
     return sortArr;
 }
 
-function pushTasks(arr) {
+function getListItems(arr) {
     return arr.map(function (elem) {
         let listItem = document.createElement('li');
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.classList.add('list__item-checkbox')
         listItem.classList.add('list__item')
         if (elem.done) {
             listItem.className = 'list__item list__item_done';
+            checkbox.checked = true;
         }
-        listItem.innerHTML = elem.text;
+        listItem.append(checkbox, elem.text);
         list.append(listItem);
         console.log('2');
-    })
+    });
 }
 
 
 sortTasks(tasks);
-pushTasks(sortArr);
+getListItems(sortArr);
 
-console.log(sortArr);
+
+
+
+// const tasks = [
+//     { text: 'Buy milk', done: false },
+//     { text: 'Pick up Tom from airport', done: false },
+//     { text: 'Visit party', done: false },
+//     { text: 'Visit doctor', done: true },
+//     { text: 'Buy meat', done: true },
+// ];
+// const getListItems = listItems => {
+//     const listElem = document.querySelector('.list');
+//     const listItemElems = listItems.sort((a, b) => a.done - b.done)
+//         .map(({ text, done }) => {
+//             const listItemElem = document.createElement('li');
+//             listItemElem.classList.add('list__item');
+//             if (done) {
+//                 listItemElem.classList.add('list__item_done');
+//             }
+//             const checkboxElem = document.createElement('input');
+//             checkboxElem.setAttribute('type', 'checkbox');
+//             checkboxElem.checked = done;
+//             checkboxElem.classList.add('list__item-checkbox');
+//             listItemElem.append(checkboxElem, text);
+//             return listItemElem;
+
+//         });
+//     listElem.append(...listItemElems);
+// };
+// getListItems(tasks);
