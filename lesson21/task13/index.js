daafda29286d4c3704a21ecbf03c8011cf36d2d4
Milkweedd <antontab91@gmail.com>
2020-04-09 02:56:02 +0300

    dsa

--- lesson21/task13/index.js
+++ lesson21/task13/index.js
@@ -1,15 +1,10 @@
-export function getSection(num) {
-    let arr = [...document.querySelectorAll('span[data-number]')];
-    let newArr = arr.reduce(function (acc, elem) {
-        if (elem.dataset.number == num) {
-            acc = elem.closest('div').dataset.section;
-        }
-        return acc;
-    }, '');
-    console.log(arr)
-    return newArr;
-
+export const getSection = num => {
+    let arr = Array.from(document.querySelectorAll('span'))
+        .reduce((acc, elem) => {
+            if (elem.dataset.number == num) {
+                acc = elem.closest('div').dataset.section;
+            }
+            return acc;
+        }, '')
+    return arr;
 }
-
-// getSection()
-
