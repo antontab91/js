f1e93d1317fdb8bb510bbfa7ddd978282ae54e6f
Milkweedd <antontab91@gmail.com>
2020-04-10 01:35:55 +0300

    as

--- lesson21/task13/index.js
+++ lesson21/task13/index.js
@@ -1,10 +1,24 @@
-export const getSection = num => {
-    let arr = Array.from(document.querySelectorAll('span'))
-        .reduce((acc, elem) => {
-            if (elem.dataset.number == num) {
-                acc = elem.closest('div').dataset.section;
-            }
-            return acc;
-        }, '')
-    return arr;
+// export const getSection = num => {
+//     let arr = Array.from(document.querySelectorAll('span'))
+//         .reduce((acc, elem) => {
+//             if (elem.dataset.number == num) {
+//                 acc = elem.closest('div').dataset.section;
+//             }
+//             return acc;
+//         }, '')
+//     return arr;
+// }
+
+export function getSection(num) {
+    let arr = [...document.querySelectorAll('span')];
+
+    let newArr = arr.reduce(function (acc, elem) {
+        if (elem.dataset.number == num) {
+            acc = elem.closest('div').dataset.section;
+        }
+        return acc;
+    });
+    return newArr
+
 }
+// console.log(getSection(3))
