342ef551df8a72331d374f8890c35658165690ce
Milkweedd <antontab91@gmail.com>
2020-04-09 03:02:00 +0300

    sa

--- lesson23/task1/index.js
+++ lesson23/task1/index.js
@@ -1,27 +1,41 @@
-const tasks = [
-    { text: 'Buy milk', done: false },
-    { text: 'Pick up Tom from airport', done: false },
-    { text: 'Visit party', done: false },
-    { text: 'Visit doctor', done: true },
-    { text: 'Buy meat', done: true },
-];
-const getListItems = listItems => {
-    const listElem = document.querySelector('.list');
-    const listItemElems = listItems.sort((a, b) => a.done - b.done)
-        .map(({ text, done }) => {
-            const listItemElem = document.createElement('li');
-            listItemElem.classList.add('list__item');
-            if (done) {
-                listItemElem.classList.add('list__item_done');
-            }
-            const checkboxElem = document.createElement('input');
-            checkboxElem.setAttribute('type', 'checkbox');
-            checkboxElem.checked = done;
-            checkboxElem.classList.add('list__item-checkbox');
-            listItemElem.append(checkboxElem, text);
-            return listItemElem;
+const emailInputElem = document.querySelector('#email');
+const passwordInputElem = document.querySelector('#password');
+const emailErrorElem = document.querySelector('.error-text_email');
+const passwordErrorElem = document.querySelector('.error-text_password');
 
-        });
-    listElem.append(...listItemElems);
-};
-getListItems(tasks);
+const isRequired = value => value ? undefined : 'Required';
+const isEmail = value => value.includes("@") ? undefined : 'Should be an email';
+
+const validatorsByField = {
+    email: [isRequired, isEmail],
+    password: [isRequired],
+}
+const validate = (fieldName, value) => {
+    const validators = validatorsByField[fieldName];
+    return validators
+        .map(validator => validator(value))
+        .filter(errorText => errorText)
+        .join(', ');
+}
+
+const onEmailChange = event => {
+    const errorText = validate('email', event.target.value);
+    emailErrorElem.textContent = errorText;
+}
+const onPasswordChange = event => {
+    const errorText = validate('password', event.target.value);
+    passwordErrorElem.textContent = errorText;
+}
+
+emailInputElem.addEventListener('input', onEmailChange);
+passwordInputElem.addEventListener('input', onPasswordChange);
+
+// const formElem = document.querySelector('.login-form');
+
+const onFormSubmit = event => {
+    event.preventDefault();
+    const formData = [...new FormData(formElem)]
+        .reduce((acc, [field, value]) => ({ ...acc, [field]: value }), {});
+    alert(JSON.stringify(formData));
+}
+formElem.addEventListener('submit', onFormSubmit);
--- lesson23/task2/index.js
+++ lesson23/task2/index.js
@@ -1,14 +1,56 @@
-export function getTitleElement() {
-    let title = document.querySelector('.title');
-    console.dir(title);
-    return title;
-}
+const generateNumbersRange = (from, to) => {
+    const result = [];
+    for (let i = from; i <= to; i++) {
+        result.push(i);
 
-export function getInputElement() {
-    let input = document.querySelector('input[type="text"]');
-    console.dir(input);
-    return input;
-}
+    }
+    return result;
+};
 
-getTitleElement();
-getInputElement()
+const getLineSeats = () => generateNumbersRange(1, 10)
+    .map(seatNumber => `
+    <div 
+        class="sector__seat" 
+        data-seat-number="${seatNumber}"
+    ></div>
+    `).join('');
+
+
+const getSectorLines = () => {
+    const seatsString = getLineSeats();
+    return generateNumbersRange(1, 10)
+        .map(lineNumber => `
+    <div 
+        class="sector__line" 
+        data-line-number="${lineNumber}"
+    >${seatsString}</div>
+    `).join('');
+};
+const arenaElem = document.querySelector('.arena');
+const renderArena = () => {
+    const linesString = getSectorLines();
+    const sectorsStrings = generateNumbersRange(1, 3).map(seatNumber => `
+    <div 
+        class="sector" 
+        data-sector-number="${seatNumber}"
+    >${linesString}</div>
+    `).join('');
+    arenaElem.innerHTML = sectorsStrings;
+};
+const onSeatSelect = event => {
+    const isSeat = event.target.classList.contains('sector__seat');
+
+    if (!isSeat) {
+        return;
+    }
+
+    // const seatNumber = event.target.dataset.seatNumber;
+    const lineNumber = event.target.parentNode.dataset.lineNumber;
+    const sectorNumber = event.target.closest('.sector').dataset.sectorNumber;
+
+    const board = document.querySelector('.board__selected-seat');
+    board.innerHTML = `S ${seatNumber} - L ${lineNumber} - S ${sectorNumber}`;
+}
+arenaElem.addEventListener('click', onSeatSelect);
+
+renderArena();
