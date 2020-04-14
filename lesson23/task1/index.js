197ffdc9408a33f0341cb8bf97a405ed07b76179
Milkweedd <antontab91@gmail.com>
2020-04-15 01:44:25 +0300

    іф

--- lesson23/task1/index.js
+++ lesson23/task1/index.js
@@ -1,41 +1,109 @@
-const emailInputElem = document.querySelector('#email');
-const passwordInputElem = document.querySelector('#password');
+// 1 Если инпут не удовлетворяет атрибуты валидации, он должен обводиться красной границей;
+
+// 2 При изменении значений в полях ввода, нужно показать сообщение про ошибку ввода.
+// Ошибку вывести под соответствующим полем ввода и отобразить красным цветом
+
+// 3 Поле  email должно быть не пустое(ошибка 'Required') и содержать символ  '@'(ошибка 'Should be an email');
+
+// 4 Поле password должно быть не пустое(ошибка 'Required')
+
+// 5 Вывести значение полей формы в  alert в виде объекта после подтверждения формы
+
+// 6 В редакторе ниже чуть более детально расписано, как можно получить данные с формы в виде объекта
+
+// // так можно получить данные формы
+// const formFields = [...new FormData(formElem)];
+// // formFields => [["email", "значение поля email"], ["password", "значение поля password"]]
+// const formData = formFields.reduce(function (acc, formField) {
+//     const prop = formField[0]; // здесь "name" инпута
+//     const value = formField[0]; // здесь "value" инпута
+//     // если использовать деструктуризацию, то можно предыдущие 2 строки записать короче
+//     // const [prop, value] = formField;
+//     return {
+//         // используем оператор расширения, чтобы в acc добвить свойства все предыдущих итераций
+//         ...acc,
+//         // используем вычислимое свойство объекта, чтобы добавить в аккумулятор новое свойство
+//         [prop]: value,
+//     };
+// }, {});
+
+
+const emailInputElement = document.querySelector('#email');
+const passwordInputElement = document.querySelector('#password');
+
 const emailErrorElem = document.querySelector('.error-text_email');
 const passwordErrorElem = document.querySelector('.error-text_password');
 
-const isRequired = value => value ? undefined : 'Required';
-const isEmail = value => value.includes("@") ? undefined : 'Should be an email';
+// console.log(emailErrorElem, emailInputElement, passwordErrorElem, passwordInputElement);
 
-const validatorsByField = {
-    email: [isRequired, isEmail],
-    password: [isRequired],
+function isRequired(value) {           // когда мы получили значение поля , нам нужно его проанализировать 
+    if (value) {                       // если поле не пустое 
+        return undefined;
+    }                                   // в противном случае ошибка (если значения нету )
+    return 'Required';
 }
+
+function isEmail(value) {               // провера является ли значение емейлом (есть ли в нем '@' )
+    if (value.includes('@')) {           // если содержит собаку    
+        return undefined;
+    }
+    return 'Should be an email';            // в противном случае возвращаем текст ошибки  
+}
+
+
+const validatorByFiled = {
+    email: [isRequired, isEmail],
+    password: [isRequired]
+};
+
 const validate = (fieldName, value) => {
-    const validators = validatorsByField[fieldName];
+    const validators = validatorByFiled[fieldName];
     return validators
         .map(validator => validator(value))
         .filter(errorText => errorText)
         .join(', ');
-}
+};
 
-const onEmailChange = event => {
-    const errorText = validate('email', event.target.value);
+function onEmailChange(event) {
+    const errorText = validate('email', event.target.value);       // вместе с функцией валидейт - єто упрощенная версия того что ниже
     emailErrorElem.textContent = errorText;
 }
-const onPasswordChange = event => {
+function onPasswordChange(event) {
     const errorText = validate('password', event.target.value);
+
     passwordErrorElem.textContent = errorText;
 }
 
-emailInputElem.addEventListener('input', onEmailChange);
-passwordInputElem.addEventListener('input', onPasswordChange);
 
-const formElem = document.querySelector('.login-form');
 
-const onFormSubmit = event => {
-    event.preventDefault();
-    const formData = [...new FormData(formElem)]
-        .reduce((acc, [field, value]) => ({ ...acc, [field]: value }), {});
-    alert(JSON.stringify(formData));
-}
-formElem.addEventListener('submit', onFormSubmit);
+// function onEmailChange(event) {         // когда будет меняться поле емейл , я хочу сгенерировать ошибку на основании єтих 2 валидаторов
+
+//     const errorText = [isRequired, isEmail]        //по двум функиям прохожусь мапом и вызываю каждую функцию со значением event.target.value
+//         .map(validator => validator(event.target.value))
+//         .filter(errorText => errorText)                        // отфильтровую , если не андефаинд - выведется текст ошибки 
+//         .join(', ');                                            // соеденяю текст ошибок с помощью метода джоин и запятой с пробелом 
+
+//     emailErrorElem.textContent = errorText;                     // вставляю сгенерированную ошибку 
+//     // console.log(event.target.value);
+// }
+
+// function onPasswordChange(event) {
+//     const errorText = [isRequired]
+//         .map(validator => validator(event.target.value))
+//         .filter(errorText => errorText)
+//         .join(', ');
+
+//     passwordErrorElem.textContent = errorText;
+// }
+
+
+emailInputElement.addEventListener('input', onEmailChange);   // на каждое изминение в импуте , я запускаю функцию , которая принимает значение обьекта ивента(event.target.value)
+passwordInputElement.addEventListener('input', onPasswordChange);
+
+
+
+
+
+
+
+
