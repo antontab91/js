9088717ece408da132058f72b0fd43ce27f5e990
Milkweedd <antontab91@gmail.com>
2020-04-16 18:09:20 +0300

    ds

--- lesson24/task4/index.js
+++ lesson24/task4/index.js
@@ -1,54 +1,37 @@
-// const students = [
-//     { name: 'Tom', birthDate: '01/15/2010' },
-//     { name: 'Ben', birthDate: '01/17/2008' },
-//     { name: 'Sam', birthDate: '03/15/2010' },
-//     { name: 'Bob', birthDate: '02/31/2008' },
-//     { name: 'Gram', birthDate: '01/11/2003' },
-//     { name: 'Alex', birthDate: '05/15/2010' }
-// ];
-// const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-// export const studentsBirthDays = students => {
-//     const sortedStudents = students.sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate());
-
-//     // console.log(new Date('01/17/2008').getDate())    // берет дату 
-//     const res = sortedStudents.reduce((acc, { name, birthDate }) => {
-//         // console.log(acc);
-//         // console.log({ name, birthDate });        // обьект
-//         const monthsName = months[new Date(birthDate).getMonth()];
-//         return { ...acc, [monthsName]: acc[monthsName] ? acc[monthsName].concat(name) : [name] };
-
-//         console.log(acc)
-//     }, {});
-
-//     return res;
-
-// };
-
-
-
-
-
-
-const students = [
+const studentsArr = [
     { name: 'Tom', birthDate: '01/15/2010' },
-    { name: 'Ben', birthDate: '05/17/2008' },
+    { name: 'Ben', birthDate: '01/17/2008' },
     { name: 'Sam', birthDate: '03/15/2010' },
-    { name: 'Bob', birthDate: '04/21/2008' },
+    { name: 'Bob', birthDate: '05/31/2008' },
     { name: 'Gram', birthDate: '01/11/2003' },
     { name: 'Alex', birthDate: '05/15/2010' }
 ];
+const mouthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-function studentsBirthDays(students) {
-    const newArrStudents = students.slice();  // создаем новій массив , чтобі не менять исходный 
-    const sortStudens = newArrStudents.sort(function (a, b) {
+function studentsBirthDays(studentsArr) {
+    const sortStudentsArr = studentsArr.slice().sort(function (a, b) {
         return new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate();
     });
 
+    let iteration = 0;
+    console.log(sortStudentsArr);
 
-    console.log(sortStudens);
-}
+    return sortStudentsArr.reduce(function (acc, { name, birthDate }) {
+        const nameOfMounth = mouthArr[new Date(birthDate).getMonth()];
+        // console.log(nameOfMounth)
+        console.log(`итерация номер ${iteration}`, acc)
+        iteration++;
+        console.log([nameOfMounth])
+        return {
+            ...acc, [nameOfMounth]: acc[nameOfMounth]          // если месяц уже имеется в акамуляторе 
+                ? acc[nameOfMounth].concat(name)          // тогда конкатенируем итмена 
+                : [name]                            // если месяца еще не было - тогда пишем масив с менеме
 
-console.log(studentsBirthDays(students));
+        };
+    }, {});
+
+    // console.log(sortStudentsArr);
+
+};
+
+console.log(studentsBirthDays(studentsArr))
