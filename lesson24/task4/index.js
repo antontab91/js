0836e8095075ad1ab28dfb13158a05ae7657a3a5
Milkweedd <antontab91@gmail.com>
2020-04-16 22:15:51 +0300

    as

--- lesson24/task4/index.js
+++ lesson24/task4/index.js
@@ -9,29 +9,56 @@ const studentsArr = [
 const mouthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 
 function studentsBirthDays(studentsArr) {
-    const sortStudentsArr = studentsArr.slice().sort(function (a, b) {
-        return new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate();
+    const returnObj = {};
+    const newArr = studentsArr.slice().sort((a, b) => {
+        new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate();
     });
+    newArr.map((elem) => {
+        console.log(new Date(elem.birthDate).getMonth());
+        const elemCount = new Date(elem.birthDate).getMonth();
+        // console.log(returnObj[mouthArr(elemCount)])
+        if (returnObj[mouthArr[elemCount]] === undefined) {
+            returnObj[mouthArr[elemCount]] = [];
+        }
+        returnObj[mouthArr[elemCount]].push(elem.name)
 
-    let iteration = 0;
-    console.log(sortStudentsArr);
+    });
+    return returnObj;
 
-    return sortStudentsArr.reduce(function (acc, { name, birthDate }) {
-        const nameOfMounth = mouthArr[new Date(birthDate).getMonth()];
-        // console.log(nameOfMounth)
-        console.log(`итерация номер ${iteration}`, acc)
-        iteration++;
-        console.log([nameOfMounth])
-        return {
-            ...acc, [nameOfMounth]: acc[nameOfMounth]          // если месяц уже имеется в акамуляторе 
-                ? acc[nameOfMounth].concat(name)          // тогда конкатенируем итмена 
-                : [name]                            // если месяца еще не было - тогда пишем масив с менеме
+}
 
-        };
-    }, {});
+console.log(studentsBirthDays(studentsArr));
+studentsBirthDays(studentsArr)
 
-    // console.log(sortStudentsArr);
+// function studentsBirthDays(studentsArr) {
+//     const sortStudentsArr = studentsArr.slice().sort(function (a, b) {
+//         return new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate();
+//     });
 
-};
+//     let iteration = 0;
+//     console.log(sortStudentsArr);
 
-console.log(studentsBirthDays(studentsArr))
+//     return sortStudentsArr.reduce(function (acc, { name, birthDate }) {
+//         const nameOfMounth = mouthArr[new Date(birthDate).getMonth()];
+//         // console.log(nameOfMounth)
+//         console.log(`итерация номер ${iteration}`, acc)
+//         iteration++;
+//         console.log([nameOfMounth])
+//         return {
+
+//             ...acc, [nameOfMounth]: acc[nameOfMounth]          // если месяц уже имеется в акамуляторе 
+//                 ? acc[nameOfMounth].concat(name)          // тогда конкатенируем итмена 
+//                 : [name]                            // если месяца еще не было - тогда пишем масив с менеме
+
+//         };
+//     }, {});
+
+//     // console.log(sortStudentsArr);
+
+// };
+
+
+
+
+
+
