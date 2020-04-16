a670e967ecf497ad89a45c539d16a398968a674a
Milkweedd <antontab91@gmail.com>
2020-04-16 06:16:57 +0300

    ds

--- lesson24/task4/index.js
+++ lesson24/task4/index.js
@@ -1,10 +1,10 @@
 const students = [
-    { name: 'James', birthDate: '11/11/1968' },
     { name: 'Tom', birthDate: '01/15/2010' },
     { name: 'Ben', birthDate: '01/17/2008' },
-    { name: 'Pen', birthDate: '02/17/2008' },
-    { name: 'Jen', birthDate: '01/18/2008' },
     { name: 'Sam', birthDate: '03/15/2010' },
+    { name: 'Bob', birthDate: '02/31/2008' },
+    { name: 'Gram', birthDate: '01/11/2003' },
+    { name: 'Alex', birthDate: '05/15/2010' }
 ];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
@@ -41,4 +41,4 @@ export const studentsBirthDays = students => {
 //     console.log(sortStudens);
 // }
 
-studentsBirthDays(students);
+console.log(studentsBirthDays(students));
