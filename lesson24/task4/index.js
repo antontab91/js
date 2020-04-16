f9b9e48f389424fe3fcd9f2c524613de3d2f8f7c
Milkweedd <antontab91@gmail.com>
2020-04-16 06:12:17 +0300

    sda

--- lesson24/task4/index.js
+++ lesson24/task4/index.js
@@ -10,10 +10,35 @@ const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', '
 
 export const studentsBirthDays = students => {
     const sortedStudents = students.sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate());
+
+    console.log(new Date('01/17/2008').getDate())
     const res = sortedStudents.reduce((acc, { name, birthDate }) => {
         const monthsName = months[new Date(birthDate).getMonth()];
         return { ...acc, [monthsName]: acc[monthsName] ? acc[monthsName].concat(name) : [name] };
     }, {});
 
     return res;
+
 };
+
+
+// const students = [
+//     { name: 'Tom', birthDate: '01/15/2010' },
+//     { name: 'Ben', birthDate: '01/17/2008' },
+//     { name: 'Sam', birthDate: '03/15/2010' },
+//     { name: 'Bob', birthDate: '02/31/2008' },
+//     { name: 'Gram', birthDate: '01/11/2003' },
+//     { name: 'Alex', birthDate: '05/15/2010' }
+// ];
+
+// const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+// function studentsBirthDays(students) {
+//     const newArrStudents = students.slice();  // создаем новій массив , чтобі не менять исходный 
+//     const sortStudens = newArrStudents.sort(function (a, b) {
+//         return new Date(a.birthDate) - new Date(b.birthDate);
+//     });
+//     console.log(sortStudens);
+// }
+
+studentsBirthDays(students);
