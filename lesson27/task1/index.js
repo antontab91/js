e35486527d14fd4d3ce6eecfcfe124e0149de6be
Milkweedd <antontab91@gmail.com>
2020-04-22 03:27:30 +0300

    s

--- lesson27/task1/index.js
+++ lesson27/task1/index.js
@@ -1,15 +1,21 @@
+localStorage.clear();
+localStorage.setItem('name', JSON.stringify('Tom'));
+localStorage.setItem('age', JSON.stringify(17));
+
+
+
+
+
+
 export const getLocalStorageData = () => {
     return Object.entries(localStorage)
         .reduce((acc, [key, value]) => {
-            let newValue;
-            try {
-                newValue = JSON.parse(value);
-            } catch (e) {
-                newValue = value;
-            }
             return {
                 ...acc,
-                [key]: newValue
+                [key]: JSON.parse(value),
             }
         }, {});
 }
+
+
+console.log(getLocalStorageData());
