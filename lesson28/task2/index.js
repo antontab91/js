0a32e697d87485530ecbc060ae40f154fc421736
Milkweedd <antontab91@gmail.com>
2020-04-27 01:39:30 +0300

    ew

--- lesson28/task2/index.js
+++ lesson28/task2/index.js
@@ -1,18 +1,41 @@
-const favorites = ['id-2'];
+const favorites = ['id-2', 'id-25'];
 const tree = {
     id: 'id-1',
     name: 'Products',
     nodes: [{
         id: 'id-2',
         name: 'Food',
-        nodes: []
-    }, ],
+        nodes: [{
+            id: 'id-22',
+            name: 'Food',
+            nodes: [{
+                id: 'id-28',
+                name: 'Food',
+                nodes: [{
+                    id: 'id-23',
+                    name: 'Food',
+                    nodes: [{
+                        id: 'id-7',
+                        name: 'Food',
+                        nodes: [{
+                            id: 'id-25',
+                            name: 'Food25',
+                            nodes: []
+                        }]
+                    }]
+                }]
+            }]
+        }]
+    }]
 };
 
 const markFavorites = (tree, favorites) => {
     const isFavorite = favorites.includes(tree.id);
+    // console.log({ ...tree })
+    console.log({ isFavorite })
+
     return {
-        ...tree,
+        ...tree,            // весь массив
         isFavorite,
         nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
     }
