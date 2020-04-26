d6f6f1135ad99f51982404d4f6a158745688dc99
Milkweedd <antontab91@gmail.com>
2020-04-26 03:20:17 +0300

    asd

--- lesson27/task3/src/scripts/index.js
+++ lesson27/task3/src/scripts/index.js
@@ -1,17 +1,8 @@
-import { setItem, getItem } from './storage.js'
-import { createNewTask } from './createTask.js';
-import { getChecked } from './updateTask.js';
-import { renderListItems, sortTask, listElem } from './renderer.js';
+import { initTodoListHandlers } from './todoList.js';
+import { renderTasks } from './renderer.js';
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    renderListItems();
+    renderTasks();
+    initTodoListHandlers();
 });
-
-const onStorageChange = e => {
-    if (e.key === 'tasksList') {
-        listElem.innerHTML = '';
-        renderListItems();
-    }
-}
-
-window.addEventListener('storage', onStorageChange)
