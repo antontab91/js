c9fa10e81e26a8f275164c31b139262d836ff0dc
Milkweedd <antontab91@gmail.com>
2020-04-28 02:35:12 +0300

    dfs

--- lesson29/task2/index.js
+++ lesson29/task2/index.js
@@ -1,13 +1,28 @@
 export const pinger = (num, period) => {
+    // const interval = setInterval(() => {
+    //     console.log('Ping');
+    // }, period);
+
+    // setTimeout(() => {
+    //     clearInterval(1);
+    // }, period * num);
+
+
+    // console.log(interval);// в переменной интервал сейчас находится идентификатор собітия веб апи
+
+
+
     let i = num;
-    console.log('Ping')
+
     const interval = setInterval(() => {
         if (--i > 0) {
-            console.log('Ping');
+            console.log('Ping')
         } else {
-            clearInterval(interval);
+            clearInterval(1);
         }
+
     }, period);
 
 };
 
+pinger(5, 1000);
