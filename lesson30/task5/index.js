712d30c279bab8e7a5b9061a5347e3c7c460f221
Milkweedd <antontab91@gmail.com>
2020-04-28 21:45:15 +0300

    sa

--- lesson30/task5/index.js
+++ lesson30/task5/index.js
@@ -1,33 +1,14 @@
-const imgUrl = 'https://s3.tproger.ru/uploads/2017/04/11-js-tricks-880x308.png';
+import { addImage } from './addImage.js'
 
-export const addImageV2 = (imgUrl) => {
-    const addImage = new Promise((resolve, reject) => {
-        const pageElem = document.querySelector('.page');
-        const sizeCont = document.querySelector('.image-size');  // чтобы прошла проверка на громкод, если я еще буду проверять, нужно эти строки закомментить 
-        const imageElem = document.createElement('img');
-        imageElem.setAttribute('alt', 'Js icon');
-        imageElem.src = imgUrl;
+export const addImageV2 = imgSrc => {
+    return new Promise((resolve, reject) => {
+        addImage(imgSrc, (error, data) => {
+            if (error) {
+                reject(error);
+                return;
+            }
 
-        pageElem.append(imageElem);
-
-        const onImageLoaded = () => {
-            const { width, height } = imageElem;
-            sizeCont.textContent = `${width} x ${height}`  // чтобы прошла проверка на громкод, если я еще буду проверять, нужно эти строки закомментить 
-            resolve({ width, height });
-        };
-        const onErrorImageLoaded = () => {
-            reject(new Error('Image load failed'));
-        }
-
-        imageElem.addEventListener('load', onImageLoaded);
-        imageElem.addEventListener('error', onErrorImageLoaded);
-
-
+            resolve(data);
+        });
     });
-    return addImage;
-}
-
-// addImage(imgUrl);
-const result = addImageV2(imgUrl);
-result.then((data) => { console.log(data) });
-result.catch((error) => { console.log(error) });
+};
