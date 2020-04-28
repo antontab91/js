8656c5b8bb75efc364460420baf43c2994d91256
Milkweedd <antontab91@gmail.com>
2020-04-29 00:54:23 +0300

    sd

--- lesson31/task1/index.js
+++ lesson31/task1/index.js
@@ -4,16 +4,17 @@ export const requestUserData = userId => {
         if (userId === 'broken') {
             setTimeout(() => {
                 reject(
-                    new Error('User is not found')
+                    new Error('User not found')
                 );
             }, 500);
         } else {
             setTimeout(() => {
                 resolve({
                     name: 'John',
-                    email: `${userId}`,
-                    userId
-                })
+                    age: 17,
+                    userId,
+                    email: `${userId}@example.com`,
+                });
             }, 1000);
         }
     });
@@ -22,15 +23,5 @@ export const requestUserData = userId => {
 };
 
 
-
-// requestUserData('user-1')
-//     .then((data) => {
-//         console.log(data);
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     })
-//     .finally(() => {
-//         console.log('finally');
-//     });
+sd
 
