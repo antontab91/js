b57ec023daa58258ea14deecee61ed4b874c4262
Milkweedd <antontab91@gmail.com>
2020-04-28 21:18:33 +0300

    ds

--- lesson30/task4/index.js
+++ lesson30/task4/index.js
@@ -3,7 +3,7 @@ const imgUrl = 'https://media.proglib.io/wp-uploads/2017/12/mxresdefault.jpg';
 export const addImage = (imgUrl) => {
     const p = new Promise((resolve, reject) => {
         const pageElem = document.querySelector('.page');
-        const sizeCont = document.querySelector('.image-size');
+        // const sizeCont = document.querySelector('.image-size');
         const imageElem = document.createElement('img');
         imageElem.setAttribute('alt', 'Js icon');
         imageElem.src = imgUrl;
@@ -12,7 +12,7 @@ export const addImage = (imgUrl) => {
 
         const onImageLoaded = () => {
             const { width, height } = imageElem;
-            sizeCont.textContent = `${width} x ${height}`
+            // sizeCont.textContent = `${width} x ${height}`
             resolve({ width, height });
         };
         const onErrorImageLoaded = () => {
