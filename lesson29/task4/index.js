6d009b596f1514fa36ece5f49769998893e13627
Milkweedd <antontab91@gmail.com>
2020-04-28 10:03:35 +0300

    sa

--- lesson29/task4/index.js
+++ lesson29/task4/index.js
@@ -1,17 +1,17 @@
+// export const requestUserData = (userId, callback) => {
+//     setTimeout(() => {
+//         if (userId === 'broken') {
+//             callback(null, 'Failed to load user data')
+//         }
+//         callback({ userId: userId, email: `${userId}@example.com` });
+//     }, 2000);
+// }
+
+
 export const requestUserData = (userId, callback) => {
     setTimeout(() => {
         if (userId === 'broken') {
             callback(null, 'Failed to load user data')
-        }
-        callback({ userId: userId, email: `${userId}@example.com` });
-    }, 2000);
-}
-
-
-export const requestUserData = (userId, callback) => {
-    setTimeout(() => {
-        if (userId === 'broken') {
-            callback(null, 'Failed to load user data');
         }
 
         callback({ userId: userId, email: `${userId}@example.com` });
