91851f139dcaeed377f626c4e0719be406bcd70e
Milkweedd <antontab91@gmail.com>
2020-04-29 01:31:12 +0300

    sa

---
+++ lesson31/task3/index.js
@@ -0,0 +1,37 @@
+const successPromise = new Promise(resolve => {
+    resolve(32);
+});
+
+/*
+ * исправьте цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+
+successPromise
+    .then(number => {
+        const halfNumber = number / 2;
+        return halfNumber;
+    })
+    .then(number => {
+        /* fix this handler */
+        const squaredNumber = number * number;
+        return squaredNumber;
+    })
+    .then(result => {
+        console.log(result); // 256
+        // return result;
+    });
+
+/*
+ * исправьте цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+successPromise
+    .then(number => {
+        /* fix this handler */
+        return number * 10;
+    })
+    .then(result => {
+        console.log(result); // 320
+        // return result;
+    });
+
+console.log('!!! Обратите внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!');
