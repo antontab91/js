8ade1c2b9a5280464e71cbe19685944e52014605
Milkweedd <antontab91@gmail.com>
2020-04-29 00:48:42 +0300

    ds

---
+++ lesson31/task1/index.js
@@ -0,0 +1,46 @@
+const requestUserData = userId => {
+    const request = new Promise((resolve, reject) => {
+
+        if (userId === 'broken') {
+            setTimeout(() => {
+                reject(
+                    new Error('User is not found')
+                );
+            }, 2000);
+        } else {
+            setTimeout(() => {
+                resolve({
+                    name: 'John',
+                    email: `${userId}`,
+                    userId
+                })
+            }, 1000);
+        }
+    });
+
+    return request;
+};
+
+
+// requestUserData('user-1')
+//     .then((data) => {
+//         console.log(data);
+//     }).catch((error) => {
+//         console.log(error);
+//     });
+
+requestUserData('user-1')
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log('finally');
+    });
+
+// requestUserData('user-1')
+//     .then((data) => {
+//         console.log(data);
+//     });
