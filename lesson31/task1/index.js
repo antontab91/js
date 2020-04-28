41681a5aa3ec3421123544fcff5b40e6981554de
Milkweedd <antontab91@gmail.com>
2020-04-29 00:55:56 +0300

    sa

--- lesson31/task1/index.js
+++ lesson31/task1/index.js
@@ -1,11 +1,8 @@
-export const requestUserData = userId => {
-    const request = new Promise((resolve, reject) => {
-
+export const requestUserData = userId =>
+    new Promise((resolve, reject) => {
         if (userId === 'broken') {
             setTimeout(() => {
-                reject(
-                    new Error('User not found')
-                );
+                reject(new Error('User not found'));
             }, 500);
         } else {
             setTimeout(() => {
@@ -19,9 +16,16 @@ export const requestUserData = userId => {
         }
     });
 
-    return request;
-};
 
 
-sd
+// requestUserData('user-1')
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+//     .finally(() => {
+//         console.log('finally');
+//     });
 
