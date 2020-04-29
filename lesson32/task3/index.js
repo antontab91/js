adcf775d32a1da4b151c472058b7e3e968bf89e4
Milkweedd <antontab91@gmail.com>
2020-04-29 18:21:07 +0300

    as

--- lesson32/task3/index.js
+++ lesson32/task3/index.js
@@ -1,5 +1,18 @@
-console.log(new Promise(() => {
+/*
+ * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
+ */
 
-}))
+/* ...code here */
 
-console.log(Promise)
+const makePromise = (num) => {
+    return Promise.resolve(num);
+}
+
+
+/*
+ * пример использования
+ */
+makePromise(17)
+    .then(number => {
+        console.log(number); // 17
+    });
