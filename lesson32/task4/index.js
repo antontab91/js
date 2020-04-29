6bae4bd55c4b79ce44ca823f58769108dc0bbede
Milkweedd <antontab91@gmail.com>
2020-04-29 18:32:30 +0300

    sd

--- lesson32/task4/index.js
+++ lesson32/task4/index.js
@@ -1,5 +1,24 @@
-console.log(new Promise(() => {
+const promiseNumber1 = Promise.resolve(67);
+const promiseNumber2 = Promise.resolve(23);
+const promiseNumber3 = Promise.resolve(8);
 
-}))
+/*
+ * создайте промис и присвойте переменной resultPromise
+ * чтобы в консоль вывелась сумма всех чисел из трех промисов
+ */
+/* ...code here */
+export const promisesCombainr = (...promiseNumbers) => {
+    return Promise.all(promiseNumbers);
+};
 
-console.log(Promise)
+export const resultPromise = promisesCombainr(promiseNumber1, promiseNumber2, promiseNumber3);
+
+resultPromise
+    .then(numbersList => {
+        console.log(numbersList);
+        const sum = numbersList.reduce((acc, num) => acc + num, 0);
+        return sum;
+    })
+    .then(result => {
+        console.log(result); // 98
+    });
