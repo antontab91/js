759b3a842535a37c8fbd7721f074dd808b788d22
Milkweedd <antontab91@gmail.com>
2020-05-05 01:28:59 +0300

    ds

--- lesson33/task2/index.js
+++ lesson33/task2/index.js
@@ -1,5 +1,26 @@
-console.log(new Promise(() => {
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks'
 
-}))
 
-console.log(Promise)
+export const getTasksList = () => {
+    return fetch(baseUrl).then((result) => {
+        return result.json();
+    })
+};
+
+// console.log(getTasksList());
+
+// export const getTaskById = (taskId) => {
+//     return fetch(`${baseUrl}`)
+//         .then(response => response.json()).then((result) => {
+//             return result[taskId - 1]
+//         });
+// };
+
+export const getTaskById = (taskId) => {
+    return fetch(`${baseUrl}/${taskId}`)
+        .then(response => response.json())
+};
+
+// console.log(getTaskById(1));
+
+
