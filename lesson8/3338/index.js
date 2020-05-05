a2214e4b2ed65cca0e591e24e796898273b187c4
Milkweedd <antontab91@gmail.com>
2020-05-05 13:31:32 +0300

    s

--- lesson8/3338/index.js
+++ lesson8/3338/index.js
@@ -1,14 +1,9 @@
-// let keyList = ['name', 'address', 'age'];
-// let valuesList = ['Bob', 'Ukraine', 56];
 
 const buildObject = (keyList, valuesList) => {
-    // let result = []
     return keyList.reduce((acc, el, index) => {
         return { ...acc, [el]: valuesList[index] }
     }, {});
 };
 
-// console.log(buildObject(keyList, valuesList));
-// buildObject(keyList, valuesList);
 
 
