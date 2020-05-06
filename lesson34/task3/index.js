e9f16b7923549691743063a54439a62e63ba6d0a
Milkweedd <antontab91@gmail.com>
2020-05-06 15:51:46 +0300

    sa

--- lesson34/task3/index.js
+++ lesson34/task3/index.js
@@ -1,5 +1,38 @@
-console.log(new Promise(() => {
+const loginForm = document.querySelector('.login-form');
+const submitBtn = document.querySelector('.submit-button');
+const errorText = document.querySelector('.error-text');
+const allUsers = 'https://5e98af0c5eabe7001681c458.mockapi.io/v1/users';
+const headersJson = { 'Content-Type': 'application/json; charset=utf-8' };
 
-}))
+const checkValidation = () => {
+    if (loginForm.reportValidity()) {
+        submitBtn.disabled = false;
+    }
+}
+const onFormSubmit = event => {
+    event.preventDefault();
+    const formData = [...new FormData(loginForm)]
+        .reduce((acc, arr) => ({...acc, [arr[0]]: arr[1] }), {});
+    addNewUser(formData)
+        .then(response => response.json())
+        .then(user => {
+            alert(JSON.stringify(user));
+            loginForm.reset();
+            submitBtn.disabled = true;
+        })
+        .catch(() => {
+            errorText.textContent = 'Failed to create user';
+            loginForm.reset();
+            submitBtn.disabled = true;
+        });
+};
+loginForm.addEventListener('submit', onFormSubmit);
+loginForm.addEventListener('input', checkValidation);
 
-console.log(Promise)
+const addNewUser = user => {
+    return fetch(allUsers, {
+        method: 'POST',
+        headers: headersJson,
+        body: JSON.stringify(user),
+    });
+};
