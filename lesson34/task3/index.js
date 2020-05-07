0157af901f17deaa20d9e31f52becae5916cf99b
Milkweedd <antontab91@gmail.com>
2020-05-07 23:50:59 +0300

    as

--- lesson34/task3/index.js
+++ lesson34/task3/index.js
@@ -24,9 +24,7 @@ const vlidationVheck = () => {
 
 const sendFormData = () => {
     event.preventDefault(); // говорю євенту что хочу чтобі он изменил дефолтное поведение 
-    // const userData = Object.fromEntries([...new FormData(loginFormElem)]);
-    const userData = [...new FormData(loginForm)]  // клонировали форму и скопировали в массив 
-        .reduce((acc, arr) => ({ ...acc, [arr[0]]: arr[1] }), {})
+    const userData = Object.fromEntries([...new FormData(loginFormElem)]);
     getAnswer(userData)
         .then((response) => {
             return response.json();
