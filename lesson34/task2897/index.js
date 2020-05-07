bb3950987271eb8617de12d3eb6e3debcc7a5fa1
Milkweedd <antontab91@gmail.com>
2020-05-07 23:59:44 +0300

    as

--- lesson34/task2897/index.js
+++ lesson34/task2897/index.js
@@ -1,38 +1,60 @@
-const loginForm = document.querySelector('.login-form');
-const submitBtn = document.querySelector('.submit-button');
-const errorText = document.querySelector('.error-text');
 const allUsers = 'https://5eb321a1974fee0016ecd32b.mockapi.io/users';
 const headersJson = { 'Content-Type': 'application/json; charset=utf-8' };
 
-const checkValidation = () => {
-    if (loginForm.reportValidity()) {
-        submitBtn.disabled = false;
-    }
-}
-const onFormSubmit = event => {
-    event.preventDefault();
-    const formData = [...new FormData(loginForm)]
-        .reduce((acc, arr) => ({ ...acc, [arr[0]]: arr[1] }), {});
-    addNewUser(formData)
-        .then(response => response.json())
-        .then(user => {
-            alert(JSON.stringify(user));
-            loginForm.reset();
-            submitBtn.disabled = true;
-        })
-        .catch(() => {
-            errorText.textContent = 'Failed to create user';
-            loginForm.reset();
-            submitBtn.disabled = true;
-        });
-};
-loginForm.addEventListener('submit', onFormSubmit);
-loginForm.addEventListener('input', checkValidation);
+const loginFormElem = document.querySelector('.login-form');
+const formInputElem = document.querySelector('.form-input');
+const submitBtnElem = document.querySelector('.submit-button');
+const errorTextElem = document.querySelector('.submit-button');
 
-const addNewUser = user => {
+//подписываемся на собітие импут  в форме 
+// подписываемся на событие сабмит  в форме 
+// считываем данніе из формы
+// если хотя бі одно поле не валидно используем метод HTMLFormElement.reportValidity()
+// отправляем данніе на сервер 
+// ответ сервера віводим в алерт в виде обьекта  
+// после успешного сохранения донных - отчищаем поля 
+// если ошибка при создании пользователя в базе - выводим ошибку в errorTextElem
+
+
+const vlidationVheck = () => {
+    loginFormElem.reportValidity() ?
+        submitBtnElem.disabled = false
+        : submitBtnElem.disabled = true;
+};
+
+const sendFormData = () => {
+    event.preventDefault(); // говорю євенту что хочу чтобі он изменил дефолтное поведение 
+    const userData = Object.fromEntries([...new FormData(loginFormElem)]);
+    getAnswer(userData)
+        .then((response) => {
+            return response.json();
+        }).then((result) => {
+            alert(JSON.stringify(result));
+            loginFormElem.reset();
+            submitBtnElem.disabled = true;
+        }).catch(() => {
+            errorTextElem.textContent = 'Failed to create user';
+            loginFormElem.reset();
+            submitBtnElem.disabled = true;
+        });
+
+}
+
+const getAnswer = (userData) => {
     return fetch(allUsers, {
         method: 'POST',
         headers: headersJson,
-        body: JSON.stringify(user),
-    });
-};
+        body: JSON.stringify(userData),
+    })
+}
+
+
+
+
+
+
+loginFormElem.addEventListener('submit', sendFormData);
+loginFormElem.addEventListener('input', vlidationVheck);
+
+
+
