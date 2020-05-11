eb8df613cca86984ff10b07cea351c063d47388a
Milkweedd <antontab91@gmail.com>
2020-05-12 00:06:33 +0300

    weq

---
+++ lesson36/task1/index.js
@@ -0,0 +1,35 @@
+// async function func() {
+//     const promise = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(17);
+//         }, 1000);
+//     });
+//     const result = await promise;
+//     console.log(result);
+//     return 1;
+// }
+
+// const result = func();
+// result.then((data) => {
+//     console.log(data);
+// });
+// console.log('hi');
+
+
+const getUser = async userId => {
+    try {
+        const response = await fetch((`https://api.github.com/users/${userId}`));
+        // console.log(response);
+        if (!response.ok) {
+            return null;
+        }
+        const userData = await response.json();
+        return userData;
+    }
+    catch (e) {
+        throw new Error('Failed to fetch user');
+    }
+};
+
+getUser('fasebook');
+// console.log(getUser('fasebook'));
