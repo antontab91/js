cb00bbffa2ad71e4a25f13e0b862518e7d502d65
Milkweedd <antontab91@gmail.com>
2020-05-14 10:52:23 +0300

    ен

--- dublikate/leson1/task1/index.js
+++ dublikate/leson1/task1/index.js
@@ -1,1 +1,17 @@
-console.log('Hello, world!')
+console.log('Hello, world!');
+
+function getDrinksWithStep(guests, step) {
+    let drinksCount = 0;
+
+    for (let i = 0; i <= guests; i++) {
+        if (i % step === 1) {
+            drinksCount = drinksCount + i;
+            console.log(drinksCount);
+        }
+    }
+
+    return drinksCount;
+
+}
+
+getDrinksWithStep(10, 3)
