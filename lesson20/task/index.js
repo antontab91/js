3f48ee4401beb9e7e3420b133647fba00e1af0b6
Milkweedd <antontab91@gmail.com>
2020-05-25 12:38:01 +0300

    t

--- lesson20/task/index.js
+++ lesson20/task/index.js
@@ -1,3 +1,22 @@
+// export class Sportsman {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   run() {
+//     console.log(`${this.name} is running`);
+//   }
+// }
+
+// export class Swimmer extends Sportsman {
+//   constructor(name, style) {
+//     super(name);
+//     this.style = style;
+//   }
+//   swim() {
+//     console.log(`${this.name} is swimming ${this.style}`);
+//   }
+// }
+
 export class Sportsman {
   constructor(name) {
     this.name = name;
@@ -5,14 +24,26 @@ export class Sportsman {
   run() {
     console.log(`${this.name} is running`);
   }
+  // toString(){
+  //   console.log('pa-bam');
+
+  // }
 }
+
+const user1 = new Sportsman('John')
+// user1.run()
 
 export class Swimmer extends Sportsman {
   constructor(name, style) {
-    super(name);
-    this.style = style;
+    super(name)
+    this.style = style
   }
   swim() {
     console.log(`${this.name} is swimming ${this.style}`);
+
   }
 }
+
+const sw1 = new Swimmer('John', 'breaststroke')
+// console.log(sw1);
+// sw1.swim()
