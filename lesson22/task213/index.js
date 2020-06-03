51bac41c55733475d0cc05dd69551575106ddbda
Milkweedd <antontab91@gmail.com>
2020-06-03 10:23:26 +0300

    ів

---
+++ lesson22/task213/index.js
@@ -0,0 +1,13 @@
+// const itemElems = document.querySelectorAll('.list-item');
+// console.log(itemElems);
+// function sayHi(event) {
+//   console.log(event.target.textContent);
+// }
+// itemElems.
+//   itemElem1.addEventListener('click', sayHi);
+
+// const itemElem2 = document.querySelector('.list-item:nth-child(2)');
+// itemElem2.addEventListener('click', sayHi);
+
+// const itemElem3 = document.querySelector('.list-item:last-child');
+// itemElem3.addEventListener('click', sayHi);
