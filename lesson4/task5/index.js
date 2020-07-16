b394312a44e0cbfc4f71fe644795c6f41ee9d741
Milkweedd <antontab91@gmail.com>
2020-07-16 20:57:31 +0300

    sa

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -18,22 +18,26 @@
 
 
 
-const m = 3;
-const n = 16;
+const m = 10
+const n = 50;
+let i = m;
 let result = 0;
 
-for (let i = m; i <= n; i++) {
-    if (i % 2 === 0 && i % 4 !== 0) {
-        result += i;
-    } else if (i % 3 === 0) {
-        result -= i;
-    } else if (i % 4 === 0) {
-        result *= i;
-    }
+for (; i <= n; i++) {
     if (i % 5 === 0) {
         console.log(i);
+        continue;
     }
-    continue;
-
-};
-
+    if (i % 2 === 0 && i % 4 !== 0) {
+        result += i;
+        continue;
+    }
+    if (i % 3 === 0) {
+        result -= i;
+        continue;
+    }
+    if (i % 4 === 0) {
+        result *= i;
+        continue;
+    }
+}
