0e156aff4d0e5fb82b6066cbe65aec7e0f0f5b0a
Milkweedd <antontab91@gmail.com>
2020-07-16 20:54:51 +0300

    ds

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -1,21 +1,39 @@
-let m = 10;
-let n = 48;
+// let m = 10;
+// let n = 48;
+// let result = 0;
+
+// for (let i = m; i <= n; i++) {
+//     if (i % 2 == 0 && i % 4 != 0) {
+//         result = result + i;
+//     } else if (i % 3 == 0) {
+//         result = result - i;
+//     } else if (i % 4 == 0) {
+//         result = result * i;
+//     }
+
+//     if (i % 5 == 0) {                 //тут вывожу число кратное 5 , даже если оно подходит подвышеуказанные условия 
+//         console.log(i);
+//     }
+// }
+
+
+
+const m = 3;
+const n = 16;
 let result = 0;
 
 for (let i = m; i <= n; i++) {
-    if (i % 2 == 0 && i % 4 != 0) {
-        result = result + i;
-    } else if (i % 3 == 0) {
-        result = result - i;
-    } else if (i % 4 == 0) {
-        result = result * i;
+    if (i % 2 === 0 && i % 4 !== 0) {
+        result += i;
+    } else if (i % 3 === 0) {
+        result -= i;
+    } else if (i % 4 === 0) {
+        result *= i;
     }
-
-    if (i % 5 == 0) {                 //тут вывожу число кратное 5 , даже если оно подходит подвышеуказанные условия 
+    if (i % 5 === 0) {
         console.log(i);
     }
-}
+    continue;
 
-
-
+};
 
