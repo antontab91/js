11974e96ace9e0b36bd86d1ff904f49e83a63008
Milkweedd <antontab91@gmail.com>
2020-08-06 17:43:37 +0300

    as

--- lesson1/task1/index.js
+++ lesson1/task1/index.js
@@ -1,19 +1,19 @@
 console.log('Hello, world!');
 
-// console.log(Boolean(undefined));
-// console.log(Boolean(null));
-// console.log(Boolean(-1));
-// console.log(Boolean(0));
-// console.log(Boolean(''));
-// console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(-1));
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(NaN));
 
-// let language = 'un';
+let language = 'un';
 
-// if (language) {
-//     console.log('fds')
-// } else {
-//     console.log('sad')
-// }
+if (language) {
+    console.log('fds')
+} else {
+    console.log('sad')
+}
 
 
 const friends = [
@@ -30,3 +30,23 @@ let oldestFriend = friends.reduce((oldest, friend) => {
 }, {});
 
 console.log(oldestFriend)
+
+
+
+// const arr = [
+//     { id: "12", value: 45 },
+//     { id: "1", value: 54 },
+//     { id: "2", value: 5 }
+// ];
+
+
+// let total = 0;
+// const myMap = (arr) => {
+//     arr.map((el) => {
+//         total = total + el.value
+//     })
+
+//     return total
+// }
+
+// console.log(myMap(arr))
