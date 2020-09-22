f66a6ca327b1e78823b20f69a9f289a38b25be05
Milkweedd <antontab91@gmail.com>
2020-09-22 06:30:47 +0300

    commit

--- lesson2/task1/index.js
+++ lesson2/task1/index.js
@@ -1,8 +1,77 @@
-const userName = 'John';
-const greeting = "I'm John";
-const _profession = 'Developer';
-const $hobby = 'Walking';
-const favouriteDessert = 'cake';
-let pet_ = 'dog';
-let superNumber;
-superNumber = 17;
+// const userName = 'John';
+// const greeting = "I'm John";
+// const _profession = 'Developer';
+// const $hobby = 'Walking';
+// const favouriteDessert = 'cake';
+// let pet_ = 'dog';
+// let superNumber;
+// superNumber = 17;
+
+// const tasks = [
+//   {
+//     id: 50,
+//     name: "valera1"
+//   },
+//   {
+//     id: 12,
+//     name: "valera2"
+//   },
+//   {
+//     id: 18,
+//     name: "valera3"
+//   },
+//   {
+//     id: 40,
+//     name: "valera4"
+//   },
+//   {
+//     id: 80,
+//     name: "valera5",
+
+//   }
+// ]
+
+
+
+// const { id, name } = tasks
+//   .find(task => task.id === 18)
+
+// console.log(id, name)
+
+// const tasks = {
+//   ann: 30,
+//   bob: 50,
+//   misha: 58,
+// };
+
+// const biggestSukaChel = Object.entries(tasks)
+//   .reduce((prev, current) => {
+//     return (prev[1] > current[1]) ? prev : current
+//   }, 0)
+
+
+// const tasksCompleted = {
+//   Ann: 29,
+//   Misha: 35,
+//   Toha: 40,
+//   Sasha: 28,
+//   Sasha: 568,
+// };
+
+// const getBigTask = (tasks) => {
+//   let max = 0;
+//   let name;
+//   for (let [key, values] of Object.entries(tasks)) {
+//     if (max < values) {
+//       max = values;
+//       name = key;
+//     }
+//   }
+//   return {
+//     name,
+//     max,
+//   };
+// }
+
+
+// console.log(getBigTask(tasksCompleted));
